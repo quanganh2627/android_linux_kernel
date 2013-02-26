7f7d8164acb44a32bb66c327bbbeb014ffa7630e
Mark Brown <broonie@opensource.wolfsonmicro.com>
2013-02-26 12:47:55 +0000

    ASoC: wm8994: Cancel delayed work for jackdet unconditionally
    BZ: 139640
    
    This avoids a potential deadlock with cancellation and races with
    completion on insertion.
    
    Ported from K3.4 BZ: 115497
    
    Orig-Change-Id: Idd19ef0f05680ba9c1e76619215383b3050ef84b
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Reviewed-on: http://android.intel.com:8080/112216
    Reviewed-by: Nilofer, Samreen <samreen.nilofer@intel.com>
    Reviewed-by: Abdullah, Omair M <omair.m.abdullah@intel.com>
    Reviewed-by: Vinnakota, Lakshmi N <lakshmi.n.vinnakota@intel.com>
    Reviewed-by: Koul, Vinod <vinod.koul@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Prusty, Subhransu S <subhransu.s.prusty@intel.com>
    Tested-by: Gupta, ArvindX K <arvindx.k.gupta@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>
    (cherry picked from commit 5fdcc0eacee753d08d07a6de531a828d1d842450)
    
    Change-Id: I3bdd34c72e354cb69b29b2d93cabfabff9ab580c
    Signed-off-by: Subhransu S. Prusty <subhransu.s.prusty@intel.com>
    Reviewed-on: http://android.intel.com:8080/133327
    Reviewed-by: Diwakar, Praveen <praveen.diwakar@intel.com>
    Reviewed-by: B, Jayachandran <jayachandran.b@intel.com>
    Reviewed-by: Koul, Vinod <vinod.koul@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Shah, Hardik T <hardik.t.shah@intel.com>
    Tested-by: Koyili, SrikanthX <srikanthx.koyili@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- sound/soc/codecs/wm8994.c
+++ sound/soc/codecs/wm8994.c
@@ -3714,6 +3714,8 @@ static irqreturn_t wm1811_jackdet_irq(int irq, void *data)
 	int reg, delay;
 	bool present;
 
+	cancel_delayed_work_sync(&wm8994->mic_work);
+
 	pm_runtime_get_sync(codec->dev);
 
 	cancel_delayed_work_sync(&wm8994->mic_complete_work);
@@ -3749,8 +3751,6 @@ static irqreturn_t wm1811_jackdet_irq(int irq, void *data)
 				      msecs_to_jiffies(delay));
 	} else {
 		dev_dbg(codec->dev, "Jack not detected\n");
-
-		cancel_delayed_work_sync(&wm8994->mic_work);
 
 		snd_soc_update_bits(codec, WM8958_MICBIAS2,
 				    WM8958_MICB2_DISCH, WM8958_MICB2_DISCH);
