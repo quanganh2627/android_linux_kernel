fc11cbb7162c0249c5639aee19fa37c4b8ea9d1b
Mika Kuoppala <mika.kuoppala@linux.intel.com>
2013-04-23 17:27:08 +0300

    drm/i915: release cursor when crtc is destroyed
    crtc is holding a reference to a cursor bo and it needs
    to be released when crtc is destroyed so that we don't leak
    the cursor bo.
    
    v2: Enhance set and move cursor so that disabled
    cursor is handled correctly (Ville Syrjälä)
    
    Change-Id: I2f0ee18807ff5c87d3a7c634dbf4f682a2cfd995
    Signed-off-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -6510,7 +6510,7 @@ static int intel_crtc_cursor_set(struct drm_crtc *crtc,
 	intel_crtc->cursor_width = width;
 	intel_crtc->cursor_height = height;
 
-	intel_crtc_update_cursor(crtc, true);
+	intel_crtc_update_cursor(crtc, intel_crtc->cursor_bo != NULL);
 
 	return 0;
 fail_unpin:
@@ -6529,7 +6529,7 @@ static int intel_crtc_cursor_move(struct drm_crtc *crtc, int x, int y)
 	intel_crtc->cursor_x = x;
 	intel_crtc->cursor_y = y;
 
-	intel_crtc_update_cursor(crtc, true);
+	intel_crtc_update_cursor(crtc, intel_crtc->cursor_bo != NULL);
 
 	return 0;
 }
@@ -7042,6 +7042,8 @@ static void intel_crtc_destroy(struct drm_crtc *crtc)
 		cancel_work_sync(&work->work);
 		kfree(work);
 	}
+
+	intel_crtc_cursor_set(crtc, NULL, 0, 0, 0);
 
 	drm_crtc_cleanup(crtc);
 
