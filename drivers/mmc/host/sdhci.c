6255211d5fa58e9556ab19a1cd64bcc6ab729342
Dong Chuanxiao <chuanxiao.dong@intel.com>
2013-07-17 09:59:11 +0800

    [PORT FROM K3.4]mmc: Tell emmc chip state to SCU via new field in Dekker mutex
    BZ: 33846
    
    SCU may access emmc while the kernel is between device suspend-resume
    state. During that period the emmc chip is detached. SCU accesses fails
    since the SCU doesn't power on the chip.
    SCU must know the status of the emmc chip. Easiest way
    is to pass the information from the kernel driver to SCU.
    
    New field in the emmc dekker mutex may be updated only when
    IA has successfully locked the mutex.
    
    Change-Id: If3b6b24503eeed2ac590043ae8ad85575357d16f
    Orig-Change-Id: I453d0b5e17eb5d5bd3df003e9cca58af889d78e0
    Signed-off-by: Ning Feiyi <feiyix.ning@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-on: http://android.intel.com:8080/123842
    Reviewed-by: Koskinen, Ilkka <ilkka.koskinen@intel.com>
    Reviewed-by: Gao, Yunpeng <yunpeng.gao@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -3379,6 +3379,14 @@ EXPORT_SYMBOL_GPL(sdhci_alloc_panic_host);
  *                                                                           *
 \*****************************************************************************/
 
+static void sdhci_set_emmc_state(struct sdhci_host *host, uint32_t state)
+{
+	/* Only if there is dekker mutex available */
+	if (!host->sram_addr)
+		return;
+	writel(state, host->sram_addr + DEKKER_EMMC_STATE);
+}
+
 #ifdef CONFIG_PM
 void sdhci_enable_irq_wakeups(struct sdhci_host *host)
 {
@@ -3445,6 +3453,8 @@ int sdhci_suspend_host(struct sdhci_host *host)
 		enable_irq_wake(host->irq);
 	}
 
+	/* Card succesfully suspended. Tell information to SCU */
+	sdhci_set_emmc_state(host, DEKKER_EMMC_CHIP_SUSPENDED);
 out:
 	sdhci_release_ownership(host->mmc);
 	return ret;
@@ -3495,6 +3505,8 @@ int sdhci_resume_host(struct sdhci_host *host)
 	if (host->flags & SDHCI_USING_RETUNING_TIMER)
 		host->flags |= SDHCI_NEEDS_RETUNING;
 
+	/* Card back in active state */
+	sdhci_set_emmc_state(host, DEKKER_EMMC_CHIP_ACTIVE);
 out:
 	sdhci_release_ownership(host->mmc);
 	return ret;
