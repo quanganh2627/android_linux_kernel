0c7945fffc2c227b9309ed6bf7b9e9e00ae94d7e
Lakshmi N Vinnakota <lakshmi.n.vinnakota@intel.com>
2013-08-06 01:14:02 +0530

    audio: new generic function for sending sst uevents
    BZ: 132116
    
    Added a new generic function for sending any SST uevent.
    It takes the event name and event buffer as input.
    
    Ported from K3.4 BZ: 128506
    
    Orig-Change-Id: If8d05ccbd7913d60b459aa4f4aa98fc49cc09073
    Signed-off-by: Lakshmi N Vinnakota <lakshmi.n.vinnakota@intel.com>
    Reviewed-on: http://android.intel.com:8080/123594
    Reviewed-by: Koul, Vinod <vinod.koul@intel.com>
    Reviewed-by: Kp, Jeeja <jeeja.kp@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Prusty, Subhransu S <subhransu.s.prusty@intel.com>
    Reviewed-by: B, Jayachandran <jayachandran.b@intel.com>
    Tested-by: Gupta, ArvindX K <arvindx.k.gupta@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>
    (cherry picked from commit f0e5efd92eaad6b813cb349382b455aa052c24bd)
    
    Change-Id: I6fa407374f9a2da034d72faa47476adaa303c8f4
    Reviewed-on: http://android.intel.com:8080/132785
    Reviewed-by: Prusty, Subhransu S <subhransu.s.prusty@intel.com>
    Reviewed-by: Diwakar, Praveen <praveen.diwakar@intel.com>
    Reviewed-by: Koul, Vinod <vinod.koul@intel.com>
    Tested-by: Gupta, ArvindX K <arvindx.k.gupta@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- sound/soc/intel/sst/sst_pvt.c
+++ sound/soc/intel/sst/sst_pvt.c
@@ -28,6 +28,7 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/kobject.h>
 #include <linux/pci.h>
 #include <linux/fs.h>
 #include <linux/firmware.h>
@@ -403,3 +404,34 @@ void sst_clean_stream(struct stream_info *stream)
 	mutex_unlock(&stream->lock);
 }
 
+/*
+ * sst_create_and_send_uevent - dynamically create, send and destroy uevent
+ * @name - Name of the uevent
+ * @envp - Event parameters
+ */
+int sst_create_and_send_uevent(char *name, char *envp[])
+{
+	struct kset *set;
+	struct kobject *obj;
+	int ret = 0;
+
+	set = kset_create_and_add("SSTEVENTS", NULL, &sst_drv_ctx->dev->kobj);
+	if (!set) {
+		pr_err("kset creation failed\n");
+		return -ENOMEM;
+	}
+	obj = kobject_create_and_add(name, &sst_drv_ctx->dev->kobj);
+	if (!obj) {
+		pr_err("kboject creation failed\n");
+		ret = -ENOMEM;
+		goto free_kset;
+	}
+	obj->kset = set;
+	ret = kobject_uevent_env(obj, KOBJ_ADD, envp);
+	if (ret)
+		pr_err("sst uevent send failed - %d\n", ret);
+	kobject_put(obj);
+free_kset:
+	kset_unregister(set);
+	return ret;
+}
