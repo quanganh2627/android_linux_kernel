a6b9c286899ef29e8a40520b2dbd01d09bbb3855
Richard A. Griffiths <Richard.A.Griffiths@intel.com>
2013-08-12 16:01:46 -0700

    scripts: add xconfig support to updatedefconfigs.sh
    BZ: 130232
    
    changed the script to use case statement instead of if/else and added
    xconfig as an option for modifying kernel configs. Updated comment on
    query display.
    
    Change-Id: I627c45e21eb29e980fffc8b34478dc0e31b0688d
    Signed-off-by: Richard A. Griffiths <Richard.A.Griffiths@intel.com>
    Reviewed-on: http://android.intel.com:8080/125075
    Tested-by: Ranostay, Matt <matt.ranostay@intel.com>
    Reviewed-by: Cohen, David A <david.a.cohen@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- scripts/updatedefconfigs.sh
+++ scripts/updatedefconfigs.sh
@@ -5,21 +5,27 @@
 # $ scripts/updatedefconfigs.sh
 #
 # For each i386_XXX_defconfig inside arch/x86/configs it will ask to run either
-# make oldconfig or make menuconfig.
+# make oldconfig, make menuconfig or make xconfig.
 # In the end all defconfigs will be updated and ready for a patch.
 
 ALLDEFCONFIGS="`ls arch/x86/configs/i386_*_defconfig`"
 
 for conf in $ALLDEFCONFIGS; do
-	echo "Updating $conf (O=oldconfig, m=menuconfig): [O/m] "
+	echo "Updating $conf (O=oldconfig, m=menuconfig, x=xconfig): [O/m/x default=O]?"
 	read -s -n1 ANSWER < /dev/tty
 	cp $conf .config
 
-	if [ "$ANSWER" = "m" ]; then
-		make ARCH=i386 menuconfig
-	else
-		make ARCH=i386 oldconfig
-	fi
+	case "$ANSWER" in
+		[mM] )
+			make ARCH=i386 menuconfig
+		;;
+		[xX] )
+			make ARCH=i386 xconfig
+		;;
+		 *)
+			make ARCH=i386 oldconfig
+		;;
+	esac
 	cp .config $conf
 done;
 
