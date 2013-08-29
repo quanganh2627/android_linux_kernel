a4c398f13ead643ce79cc30cbbb9cac08a6cbe34
Mustapha Ben Zoubeir <mustaphax.ben.zoubeir@intel.com>
2013-08-29 14:38:55 +0200

    check the dlci pointer in chars_in_buffer
    BZ: 135713
    
    In case of line discipline detach while a mux client has
    received a POLL_IN event, the chars_in_buffer function
    now check the dlci pointer value to avoid kernel crash.
    
    Change-Id: Ia60cb0b1b5e4eaae01a7bccada6a7a86f2d05227
    orig-Change-Id: I9ddd8769b2bc688196e4501472f80521030227d7
    Signed-off-by: Gwenn Bourree <gwenn.bourree@intel.com>
    Signed-off-by: Nicolas LOUIS <nicolasx.louis@intel.com>
    Reviewed-on: http://android.intel.com:8080/128582
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/tty/n_gsm.c
+++ drivers/tty/n_gsm.c
@@ -3296,9 +3296,16 @@ static int gsmtty_write_room(struct tty_struct *tty)
 
 static int gsmtty_chars_in_buffer(struct tty_struct *tty)
 {
-	struct gsm_dlci *dlci = tty->driver_data;
-	if (dlci->state == DLCI_CLOSED)
-		return -EINVAL;
+	struct gsm_dlci *dlci;
+
+	if (!tty)
+		return 0;
+	else
+		dlci = tty->driver_data;
+
+	if (!dlci)
+		return 0;
+
 	return kfifo_len(dlci->fifo);
 }
 
