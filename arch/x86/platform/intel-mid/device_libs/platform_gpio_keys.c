e139db74314912e5dd3fd365be681462a232c574
Li, Ning <ning.li@intel.com>
2013-08-30 03:30:09 +0800

    [PORT FROM K3.4] gpio-keys: Port keypad driver from K3.4
    BZ: 134708
    
    This patch ports keypad driver from K3.4 to K3.10.
    Including following commits:
    adebcb45 disable auto repeat for BYT powerbtn key
    545a8456 enable volume key on byt
    946a3388 support Keyboard less system on BYT
    f1e3b8c7 gpio-key: free irq before enter S3
    
    Change-Id: I967d865975146a6b9db9c0e6c27ec8a097c9187c
    Signed-off-by: Li, Ning <ning.li@intel.com>
    Reviewed-on: http://android.intel.com:8080/128676
    Reviewed-by: Wang, Yiyang <yiyang.wang@intel.com>
    Reviewed-by: Yang, Bin <bin.yang@intel.com>
    Reviewed-by: Zhong, Huiquan <huiquan.zhong@intel.com>
    Reviewed-by: Du, Alek <alek.du@intel.com>
    Tested-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_gpio_keys.c
+++ arch/x86/platform/intel-mid/device_libs/platform_gpio_keys.c
@@ -16,6 +16,17 @@
 #include <linux/gpio.h>
 #include <linux/gpio_keys.h>
 #include <linux/platform_device.h>
+#include <linux/acpi.h>
+#include <linux/acpi_gpio.h>
+#include <linux/gpio.h>
+#include <linux/gpio_keys.h>
+#include <linux/input.h>
+#include <linux/platform_device.h>
+#include <linux/pnp.h>
+#include <asm/intel_vlv2.h>
+#include <asm/io_apic.h>
+#include <asm/acpi.h>
+#include <asm/hw_irq.h>
 #include <asm/intel-mid.h>
 #include "platform_gpio_keys.h"
 
@@ -37,6 +48,15 @@ static struct gpio_keys_button gpio_button[] = {
 	{SW_KEYPAD_SLIDE,	-1, 1, "MagSw2",	EV_SW,  0, 20},
 	{KEY_CAMERA,		-1, 1, "cam_capture",	EV_KEY, 0, 20},
 	{KEY_CAMERA_FOCUS,	-1, 1, "cam_focus",	EV_KEY, 0, 20},
+	{KEY_MENU,              -1, 1, "fp_menu_key",   EV_KEY, 0, 20},
+	{KEY_HOME,              -1, 1, "fp_home_key",   EV_KEY, 0, 20},
+	{KEY_SEARCH,            -1, 1, "fp_search_key", EV_KEY, 0, 20},
+	{KEY_BACK,              -1, 1, "fp_back_key",   EV_KEY, 0, 20},
+	{KEY_VOLUMEUP,          -1, 1, "volume_up",     EV_KEY, 0, 20},
+	{KEY_VOLUMEDOWN,        -1, 1, "volume_down",   EV_KEY, 0, 20},
+	{SW_MUTE,               -1, 1, "mute_enable",   EV_SW,  0, 20},
+	{KEY_CAMERA,            -1, 1, "camera0_sb1",   EV_KEY, 0, 20},
+	{KEY_CAMERA_FOCUS,      -1, 1, "camera0_sb2",   EV_KEY, 0, 20},
 };
 
 static struct gpio_keys_platform_data gpio_keys = {
@@ -82,3 +102,128 @@ static int __init pb_keys_init(void)
 	return 0;
 }
 late_initcall(pb_keys_init);
+
+#ifdef	CONFIG_ACPI
+enum {
+	NON_REPEAT_KEYS,
+	AUTO_REPEAT_KEYS,
+	KEY_TYPE_NUMS,
+};
+
+static struct gpio_keys_button lesskey_button_non_repeat[] = {
+	{KEY_POWER,		-1, 1, "power_btn", EV_KEY, .acpi_idx = 0, 1},
+	{ },
+};
+
+static struct gpio_keys_button lesskey_button_auto_repeat[] = {
+	{KEY_HOME,		-1, 1, "home_btn",	EV_KEY, .acpi_idx = 1},
+	{KEY_VOLUMEUP,		-1, 1, "volume_up",	EV_KEY, .acpi_idx = 2},
+	{KEY_VOLUMEDOWN,	-1, 1, "volume_down",	EV_KEY, .acpi_idx = 3},
+	{KEY_RO,		-1, 1, "rotationlock",	EV_KEY, .acpi_idx = 4},
+};
+
+struct gpio_keys_init_data {
+	struct gpio_keys_button *keys_button;
+	int nkeys;
+};
+
+static struct gpio_keys_init_data lesskey_init_data[KEY_TYPE_NUMS] = {
+	{
+		.keys_button = lesskey_button_non_repeat,
+		.nkeys = 1,
+	}, {
+		.keys_button = lesskey_button_auto_repeat,
+		.nkeys = sizeof(lesskey_button_auto_repeat) /
+			 sizeof(struct gpio_keys_button),
+	},
+};
+
+static struct gpio_keys_platform_data lesskey_keys[KEY_TYPE_NUMS] = {
+	{
+		.buttons	= lesskey_button_non_repeat,
+		.rep		= 0,
+		.nbuttons	= 0,
+	}, {
+		.buttons	= lesskey_button_auto_repeat,
+		.rep		= 1,
+		.nbuttons	= 0,
+	},
+};
+
+static struct platform_device lesskey_device[KEY_TYPE_NUMS] = {
+	{
+		.name		= "gpio-lesskey-nrpt",
+		.id		= -1,
+		.dev		= {
+			.platform_data	= &lesskey_keys[NON_REPEAT_KEYS],
+		},
+	}, {
+		.name		= "gpio-lesskey-rpt",
+		.id		= -1,
+		.dev		= {
+			.platform_data	= &lesskey_keys[AUTO_REPEAT_KEYS],
+		},
+	},
+};
+
+static int
+lesskey_pnp_probe(struct pnp_dev *pdev, const struct pnp_device_id *id)
+{
+	int type, i, num, good;
+	struct gpio_keys_button *gb;
+	struct acpi_gpio_info info;
+	int ret = 0;
+
+	for (type = 0; type < KEY_TYPE_NUMS; type++) {
+		good = 0;
+		gb = lesskey_init_data[type].keys_button;
+		num = lesskey_init_data[type].nkeys;
+		pr_info("%s, num = %d\n", __func__, num);
+
+		for (i = 0; i < num; i++) {
+			gb[i].gpio = acpi_get_gpio_by_index(&pdev->dev,
+							gb[i].acpi_idx, &info);
+			pr_info("%s lesskey [%2d]: name = %s, gpio = %d\n",
+				 (type == NON_REPEAT_KEYS) ?
+				 "non repeat" : "auto repeat",
+				 i, gb[i].desc, gb[i].gpio);
+			if (gb[i].gpio < 0)
+				continue;
+			if (i != good)
+				gb[good] = gb[i];
+			good++;
+		}
+
+		if (good) {
+			lesskey_keys[type].nbuttons = good;
+			ret = platform_device_register(&lesskey_device[type]);
+			if (ret) {
+				dev_err(&pdev->dev, "register platform device %s failed\n",
+					lesskey_device[type].name);
+				return ret;
+			}
+		}
+	}
+
+	return 0;
+}
+
+static const struct pnp_device_id lesskey_pnp_match[] = {
+	{ "INTCFD9", 0},
+	{ }
+};
+MODULE_DEVICE_TABLE(pnp, lesskey_pnp_match);
+
+static struct pnp_driver lesskey_pnp_driver = {
+	.name		= "lesskey",
+	.id_table	= lesskey_pnp_match,
+	.probe          = lesskey_pnp_probe,
+};
+
+static int __init lesskey_init(void)
+{
+	return pnp_register_driver(&lesskey_pnp_driver);
+}
+
+late_initcall(lesskey_init);
+#endif
