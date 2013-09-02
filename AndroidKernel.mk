000ba5ab3389ff49e1fb7993904198461134c64b
Quentin Casasnovas <quentinx.casasnovas@intel.com>
2013-09-02 16:49:04 +0200

    build: Generate ctags/tags/cscope from Android environment
    BZ: 135345
    
    Small change to AndroidKernel.mk so we can run "make cscope" (and the like)
    from top level directory. Also works with out of tree module, using their name
    as defined when calling the build_kernel_module Makefile function.
    Ex:
      croot
      make bcm4334x_cscope -j24
    
    A symlink in the source tree to the generated files is also automatically
    created so editors can pick it up automatically. Thanks J. Falempe for helping
    in simplifying :)
    
    Change-Id: I82e0b59577579adc5e8866cd777e9106dbe4df81
    Signed-off-by: Quentin Casasnovas <quentinx.casasnovas@intel.com>
    Reviewed-on: http://android.intel.com:8080/136839
    Reviewed-by: Koskinen, Ilkka <ilkka.koskinen@intel.com>
    Reviewed-by: Falempe, Jocelyn <jocelyn.falempe@intel.com>
    Tested-by: Falempe, Jocelyn <jocelyn.falempe@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -133,6 +133,16 @@ menuconfig xconfig gconfig: $(KERNEL_CONFIG)
 	@echo $(KERNEL_DIFFCONFIG) has been modified !
 	@echo ===========
 
+TAGS_files := TAGS
+tags_files := tags
+gtags_files := GTAGS GPATH GRTAGS GSYMS
+cscope_files := $(addprefix cscope.,files out out.in out.po)
+
+TAGS tags gtags cscope: $(KERNEL_CONFIG)
+	@$(KERNEL_BLD_ENV) $(MAKE) -C $(KERNEL_SRC_DIR) $(KERNEL_BLD_FLAGS) $@
+	@rm -f $(KERNEL_SRC_DIR)/$($@_files)
+	@cp -fs $(addprefix `pwd`/$(KERNEL_OUT_DIR)/,$($@_files)) $(KERNEL_SRC_DIR)/
+
 #used to build out-of-tree kernel modules
 #$(1) is source path relative Android top, $(2) is module name
 #$(3) is extra flags
@@ -147,6 +157,11 @@ $(2): build_bzImage
 $(2)_clean:
 	@echo Cleaning kernel module $(2) in $(1)
 	@$(KERNEL_BLD_ENV) $(MAKE) -C $(KERNEL_SRC_DIR) $(KERNEL_BLD_FLAGS) M=../../$(1) clean
+
+$(addprefix $(2)_,TAGS tags gtags cscope): $(KERNEL_CONFIG)
+	@$(KERNEL_BLD_ENV) $(MAKE) -C $(KERNEL_SRC_DIR) $(KERNEL_BLD_FLAGS) M=../../$(1) $$(subst $(2)_,,$$@)
+	@rm -f $(1)/$$($$(subst $(2)_,,$$@)_files)
+	@cp -fs $$(addprefix `pwd`/$(KERNEL_OUT_DIR)/,$$($$(subst $(2)_,,$$@)_files)) $(1)/
 
 copy_modules_to_root: $(2)
 
