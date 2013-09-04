2bf44522bd16130f0c4d8570e99355e19343ee07
Omair Mohammed Abdullah <omair.m.abdullah@linux.intel.com>
2013-09-04 20:45:33 +0530

    audio: byt: remove redundant IOAPIC programming
    BZ: 142302
    
    The IOAPIC will be programmed based on the DSDT entry for the interrupt by the
    kernel core.
    
    Change-Id: Iadef5aaa36f9583572b34c7bab241ad4de039134
    Orig-Change-Id: I49ab3b920213d2a9c048fa1619b3b0b3e2cf7513
    Signed-off-by: Omair Mohammed Abdullah <omair.m.abdullah@linux.intel.com>
    Reviewed-on: http://android.intel.com:8080/136214
    Reviewed-by: Abdullah, Omair M <omair.m.abdullah@intel.com>
    Tested-by: Koyili, SrikanthX <srikanthx.koyili@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_byt_audio.c
+++ arch/x86/platform/intel-mid/device_libs/platform_byt_audio.c
@@ -11,30 +11,8 @@
  */
 
 #include <linux/kernel.h>
-#include <linux/interrupt.h>
 #include <linux/init.h>
-#include <linux/sfi.h>
-#include <linux/platform_device.h>
-#include <asm/intel-mid.h>
 #include <asm/platform_sst_audio.h>
-#include <asm/platform_byt_audio.h>
-
-#define SST_IRQ	29
-
-static inline int byt_program_ioapic(int irq, int trigger, int polarity)
-{
-	struct io_apic_irq_attr irq_attr;
-	int ioapic;
-
-	ioapic = mp_find_ioapic(irq);
-	if (ioapic < 0)
-		return -EINVAL;
-	irq_attr.ioapic = ioapic;
-	irq_attr.ioapic_pin = irq;
-	irq_attr.trigger = trigger;
-	irq_attr.polarity = polarity;
-	return io_apic_set_pci_routing(NULL, irq, &irq_attr);
-}
 
 static int __init byt_audio_platform_init(void)
 {
@@ -46,13 +24,6 @@ static int __init byt_audio_platform_init(void)
 	ret = add_sst_platform_device();
 	if (ret < 0) {
 		pr_err("%s failed to sst_platform device\n", __func__);
-		return 0;
-	}
-
-	/* configure SST IRQ */
-	ret = byt_program_ioapic(SST_IRQ, 0, 1);
-	if (ret) {
-		pr_err("%s: ioapic programming failed", __func__);
 		return 0;
 	}
 
