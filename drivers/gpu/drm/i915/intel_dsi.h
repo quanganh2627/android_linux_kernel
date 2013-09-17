16ce16de8611331778f9d8a0352d14594eaf442a
Shobhit Kumar <shobhit.kumar@intel.com>
2013-09-17 20:17:21 +0530

    drm/i915: Add more dev ops for MIPI sub encoder
    Also add new filed in intel_dsi to have all dphy related parameters.
    These will be useful even when we go for pur generic MIPI design
    
    Change-Id: I269a6224b159cad3fbab5570999953fb595f7eba
    Signed-off-by: Shobhit Kumar <shobhit.kumar@intel.com>

--- drivers/gpu/drm/i915/intel_dsi.h
+++ drivers/gpu/drm/i915/intel_dsi.h
@@ -28,16 +28,83 @@
 #include <drm/drm_crtc.h>
 #include "intel_drv.h"
 
+#define dsi_18Bpp_loosely_packed	0
+#define dsi_18Bpp_packed		1
+#define dsi_16Bpp_packed		2
+#define dsi_24Bpp_packed		3
+
+#define DSI_18BPP_LOOSELY_PACKED		0
+#define DSI_18BPP_PACKED				1
+#define DSI_16BPP_PACKED				2
+#define DSI_24BPP_PACKED				3
+
+#define ColorConversionInBridge		0
+#define ColorConversionInHostController	1
+
+#define DSI_VIDEO_MODE		0
+#define DSI_CMD_MODE		1
+
+#define DSI_VIDEO_NBURST_SPULSE		1
+#define DSI_VIDEO_NBURST_SEVENT		2
+#define DSI_VIDEO_BURST			3
+
+#define DSI_HSS_PACKET_SIZE 4
+#define DSI_HSE_PACKET_SIZE 4
+#define DSI_HSA_PACKET_EXTRA_SIZE 6
+#define DSI_HBP_PACKET_EXTRA_SIZE 6
+#define DSI_HACTIVE_PACKET_EXTRA_SIZE 6
+#define DSI_HFP_PACKET_EXTRA_SIZE 6
+#define DSI_HFP_PACKET_EXTRA_SIZE 6
+#define DSI_EOTP_PACKET_SIZE 4
+
 struct intel_dsi_device {
 	unsigned int panel_id;
 	const char *name;
 	int type;
+	unsigned int lane_count;
 	const struct intel_dsi_dev_ops *dev_ops;
 	void *dev_priv;
+	u8 eotp_pkt;
+	u16 dsi_clock_freq;
+	u8 operation_mode;
+	u8 video_mode_type;
+	u32 pixel_format;
+	u32 data_width;
+	u8 dither;
+	u32 port_bits;
+	u8 escape_clk_div;
+	u32 lp_rx_timeout;
+	u8 turn_arnd_val;
+	u16 init_count;
+	u16 rst_timer_val;
+	u16 hs_to_lp_count;
+	u16 lp_byte_clk;
+	u32 bw_timer;
+	u16 clk_lp_to_hs_count;
+	u16 clk_hs_to_lp_count;
+	u32 video_frmt_cfg_bits;
+	u32 dphy_reg;
+
+	u8 backlight_off_delay; /*in ms*/
+	bool send_shutdown;
+	u8 shutdown_pkt_delay; /*in ms*/
 };
 
 struct intel_dsi_dev_ops {
 	bool (*init)(struct intel_dsi_device *dsi);
+
+	void (*get_info)(int pipe, struct drm_connector *connector);
+
+	void (*create_resources)(struct intel_dsi_device *dsi);
+
+	void (*dpms)(struct intel_dsi_device *dsi, bool enable);
+
+	void (*panel_reset)(struct intel_dsi_device *dsi);
+
+	void (*disable_panel_power)(struct intel_dsi_device *dsi);
+
+	/* send one time programmable commands */
+	void (*send_otp_cmds)(struct intel_dsi_device *dsi);
 
 	/* This callback must be able to assume DSI commands can be sent */
 	void (*enable)(struct intel_dsi_device *dsi);
@@ -63,6 +130,8 @@ struct intel_dsi_dev_ops {
 	struct drm_display_mode *(*get_modes)(struct intel_dsi_device *dsi);
 
 	void (*destroy) (struct intel_dsi_device *dsi);
+
+	void (*dump_regs)(struct intel_dsi_device *dsi);
 };
 
 struct intel_dsi {
