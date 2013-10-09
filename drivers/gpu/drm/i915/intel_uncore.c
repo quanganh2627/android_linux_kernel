8c0651f083494291e80aa54e8c407b2952838caa
Deepak S <deepak.s@intel.com>
2013-10-09 16:08:57 +0530

    drm/i915: Enabling read of GPU ring timestamp from GPU registers.
    BZ: 143870
    
    Split RING_TIMESTAMP into two 32 bit register (HI and LO words).
    Read RING_TIMESTAMP_LO and RING_TIMESTAMP_HI register to get GPU ring
    timestamp.
    
    Change-Id: Ie17b005056637c6f85142363e5674c1094aeda3e
    Signed-off-by: Deepak S <deepak.s@intel.com>
    Reviewed-on: http://android.intel.com:8080/137205
    Reviewed-by: Kamble, Sagar A <sagar.a.kamble@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Sun, Hang L <hang.l.sun@intel.com>
    Tested-by: Sun, Hang L <hang.l.sun@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/gpu/drm/i915/intel_uncore.c
+++ drivers/gpu/drm/i915/intel_uncore.c
@@ -505,7 +505,8 @@ static const struct register_whitelist {
 	uint32_t size;
 	uint32_t gen_bitmask; /* support gens, 0x10 for 4, 0x30 for 4 and 5, etc. */
 } whitelist[] = {
-	{ RING_TIMESTAMP(RENDER_RING_BASE), 8, 0xF0 },
+	{ RING_TIMESTAMP_LO(RENDER_RING_BASE), 4, 0xF0 },
+	{ RING_TIMESTAMP_HI(RENDER_RING_BASE), 4, 0xF0 }
 };
 
 int i915_reg_read_ioctl(struct drm_device *dev,
@@ -530,6 +531,7 @@ int i915_reg_read_ioctl(struct drm_device *dev,
 		reg->val = I915_READ64(reg->offset);
 		break;
 	case 4:
+
 		reg->val = I915_READ(reg->offset);
 		break;
 	case 2:
