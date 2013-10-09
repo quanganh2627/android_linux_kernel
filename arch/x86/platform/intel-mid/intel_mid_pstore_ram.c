4914a3752b5e43b19dd8e12bed5c1f511d67243e
Jeremy Rocher <jeremy.rocher@intel.com>
2013-10-09 14:54:00 +0200

    intel-mid, pstore_ram: increase console size to 2M
    BZ: 142128
    
    pstore ram is set to 2 Mo. Clean log messages.
    
    Change-Id: Ie278eebc46a57ea6e0e7175abbfd31137a717f07
    Signed-off-by: Jeremy Rocher <jeremy.rocher@intel.com>
    Reviewed-on: http://android.intel.com:8080/136340
    Reviewed-by: Englund, Hakan <hakan.englund@intel.com>
    Reviewed-by: Guerard, Christophe <christophe.guerard@intel.com>
    Tested-by: Guerard, Christophe <christophe.guerard@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- arch/x86/platform/intel-mid/intel_mid_pstore_ram.c
+++ arch/x86/platform/intel-mid/intel_mid_pstore_ram.c
@@ -19,13 +19,13 @@
 #include <linux/bootmem.h>
 
 #define SZ_4K	0x00001000
-#define SZ_1M	0x00100000
 #define SZ_2M	0x00200000
+#define SZ_2_1M	0x00219000
 #define SZ_16M	0x01000000
 
 /* Board files use the following if they are ok with 16M start defaults */
 #define PSTORE_RAM_START_DEFAULT	SZ_16M
-#define PSTORE_RAM_SIZE_DEFAULT		SZ_2M
+#define PSTORE_RAM_SIZE_DEFAULT		SZ_2_1M
 
 #ifdef CONFIG_X86_32
 #define RAM_MAX_MEM (max_low_pfn << PAGE_SHIFT)
@@ -37,8 +37,8 @@ static struct ramoops_platform_data pstore_ram_data = {
 	.mem_size	= PSTORE_RAM_SIZE_DEFAULT,
 	.mem_address	= PSTORE_RAM_START_DEFAULT,
 	.record_size	= SZ_4K,
-	.console_size	= SZ_1M,
-	.ftrace_size	= SZ_4K,
+	.console_size	= SZ_2M,
+	.ftrace_size	= 2*SZ_4K,
 	.dump_oops	= 1,
 };
 
@@ -63,10 +63,10 @@ static int __init intel_mid_pstore_ram_register(void)
 
 	ret = platform_device_register(&pstore_ram_dev);
 	if (ret) {
-		pr_err("%s: unable to register pstore_ram device:", __func__);
-		pr_err("start=0x%32x", (u32)pstore_ram_data.mem_address);
-		pr_err("size=0x%32x", (u32)pstore_ram_data.mem_size);
-		pr_err("ret=%d\n", ret);
+		pr_err("%s: unable to register pstore_ram device: "
+		       "start=0x%llx, size=0x%lx, ret=%d\n", __func__,
+		       (unsigned long long)pstore_ram_data.mem_address,
+		       pstore_ram_data.mem_size, ret);
 	}
 
 	return ret;
@@ -90,13 +90,17 @@ void __init pstore_ram_reserve_memory(void)
 
 	ret = memblock_reserve(mem, size);
 	if (ret) {
-		pr_err("Failed to reserve memory from %08lx-%08lx\n",
-			(long)mem, (long)(mem + size - 1));
+		pr_err("Failed to reserve memory from 0x%llx-0x%llx\n",
+		       (unsigned long long)mem,
+		       (unsigned long long)(mem + size - 1));
 		return;
 	}
 
 	pstore_ram_data.mem_address = mem;
 	pstore_ram_data.mem_size = size;
+
+	pr_info("reserved RAM buffer (0x%zx@0x%llx)\n",
+		size, (unsigned long long)mem);
 
 	intel_mid_pstore_ram_inited = true;
 }
