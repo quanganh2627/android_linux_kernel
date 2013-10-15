e3c0f73ecd7fc54d04a12a82709b67e8dea33fce
Ziyu Jiang <ziyux.jiang@intel.com>
2013-10-15 14:00:17 +0800

    Platform: Enable VRF for ov9724 sensor power on merr.
    BZ: 145125
    
    Enable voltage regulator framework for sensor ov9724 power management
    on merr platform, this is because ov9724 share the same one power source,
    direct power-on/off operation to one sensor will impact the another one if
    without VRF.
    
    Change-Id: I8c520a7e7d4a4919d5953a3bc6409d1de94b8326
    Orig-Change-Id: I3ede3f7bd401298171f51346d8f48fb22a77b48b
    Signed-off-by: Ziyu Jiang <ziyux.jiang@intel.com>
    Acked-by: Jukka Kaartinen <jukka.o.kaartinen@intel.com>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Antti Laakso <antti.laakso@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_ov9724.c
+++ arch/x86/platform/intel-mid/device_libs/platform_ov9724.c
@@ -13,6 +13,7 @@
 #include <linux/gpio.h>
 #include <linux/delay.h>
 #include <linux/atomisp_platform.h>
+#include <linux/regulator/consumer.h>
 #include <asm/intel_scu_ipcutil.h>
 #include <asm/intel-mid.h>
 #include <media/v4l2-subdev.h>
@@ -23,6 +24,8 @@
 static int camera_reset;
 static int camera_power_down;
 static int camera_vprog1_on;
+static struct regulator *vprog1_reg;
+#define VPROG1_VAL 2800000
 /*
  * MRFLD VV secondary camera sensor - OV9724 platform data
  */
@@ -65,14 +68,14 @@ static int ov9724_power_ctrl(struct v4l2_subdev *sd, int flag)
 
 	if (flag) {
 		if (!camera_vprog1_on) {
-			ret = intel_scu_ipc_msic_vprog1(1);
+			ret = regulator_enable(vprog1_reg);
 			if (!ret)
 				camera_vprog1_on = 1;
 			return ret;
 		}
 	} else {
 		if (camera_vprog1_on) {
-			ret = intel_scu_ipc_msic_vprog1(0);
+			ret = regulator_disable(vprog1_reg);
 			if (!ret)
 				camera_vprog1_on = 0;
 			return ret;
@@ -88,11 +91,37 @@ static int ov9724_csi_configure(struct v4l2_subdev *sd, int flag)
 		ATOMISP_INPUT_FORMAT_RAW_10, atomisp_bayer_order_bggr, flag);
 }
 
+static int ov9724_platform_init(struct i2c_client *client)
+{
+	int ret;
+
+	vprog1_reg = regulator_get(&client->dev, "vprog1");
+	if (IS_ERR(vprog1_reg)) {
+		dev_err(&client->dev, "regulator_get failed\n");
+		return PTR_ERR(vprog1_reg);
+	}
+	ret = regulator_set_voltage(vprog1_reg, VPROG1_VAL, VPROG1_VAL);
+	if (ret) {
+		dev_err(&client->dev, "regulator voltage set failed\n");
+		regulator_put(vprog1_reg);
+	}
+	return ret;
+}
+
+static int ov9724_platform_deinit(void)
+{
+	regulator_put(vprog1_reg);
+
+	return 0;
+}
+
 static struct camera_sensor_platform_data ov9724_sensor_platform_data = {
 	.gpio_ctrl      = ov9724_gpio_ctrl,
 	.flisclk_ctrl   = ov9724_flisclk_ctrl,
 	.power_ctrl     = ov9724_power_ctrl,
 	.csi_cfg        = ov9724_csi_configure,
+	.platform_init = ov9724_platform_init,
+	.platform_deinit = ov9724_platform_deinit,
 };
 
 void *ov9724_platform_data(void *info)
