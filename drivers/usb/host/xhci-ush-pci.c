ea06cfe5616c9bcffb59ca65570f4e04f0796f55
feng wang <feng.a.wang@intel.com>
2013-10-24 11:03:38 +0800

    usb hsic: enable lower power after hsic is disabled
    BZ: 143022
    
    When hsic_enable is called to disable HSIC, auto
    suspend is disabled. This will keep USH device in
    D0. Enable it when HSIC is disabled.
    
    Change-Id: I85af08dd08f79f7bbd4b1fc7c7f21bf848c811a4
    Signed-off-by: feng wang <feng.a.wang@intel.com>

--- drivers/usb/host/xhci-ush-pci.c
+++ drivers/usb/host/xhci-ush-pci.c
@@ -394,7 +394,7 @@ static void hsicdev_remove(struct usb_device *udev)
 		mutex_lock(&hsic.hsic_mutex);
 		hsic.modem_dev = NULL;
 		mutex_unlock(&hsic.hsic_mutex);
-		usb_disable_autosuspend(hsic.rh_dev);
+		usb_enable_autosuspend(hsic.rh_dev);
 	}
 }
 
@@ -521,13 +521,13 @@ static void ush_hsic_port_disable(void)
 	if (hsic.modem_dev) {
 		dev_dbg(&pci_dev->dev,
 			"Disable auto suspend in port disable\n");
-		usb_disable_autosuspend(hsic.modem_dev);
-		usb_disable_autosuspend(hsic.rh_dev);
+		usb_enable_autosuspend(hsic.modem_dev);
+		usb_enable_autosuspend(hsic.rh_dev);
 	}
 	if (hsic.rh_dev) {
 		dev_dbg(&pci_dev->dev,
 			"%s----> disable port\n", __func__);
-		usb_disable_autosuspend(hsic.rh_dev);
+		usb_enable_autosuspend(hsic.rh_dev);
 		clear_port_feature(hsic.rh_dev, HSIC_USH_PORT,
 				USB_PORT_FEAT_POWER);
 	}
