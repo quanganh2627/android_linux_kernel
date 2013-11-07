16e9461b78b0c63c070f7995c2fbf841f94e484b
Wang, Yu <yu.y.wang@intel.com>
2013-11-07 21:36:14 +0800

    dwc-otg: Fix ACA-Dock can't work when battery lower than 20%.
    BZ: 150147
    
    When battery lower than 20%, EM will notify USB OTG driver that VBus
    can't be drive. USB OTG driver will giveup to start host mode when
    battery is low. But haven't consider the ACA-Dock case.
    
    This patch is add one more condition for low battery case. If charger
    type is ACA-Dock, then can be ignore the low battery. Because ACA-Dock
    will drive VBus for its upstream port.
    
    Change-Id: I02ca1c2761baa6154b33fc22a454426303a335b5
    Signed-off-by: Wang, Yu <yu.y.wang@intel.com>

--- drivers/usb/dwc3/otg.c
+++ drivers/usb/dwc3/otg.c
@@ -611,7 +611,11 @@ static enum dwc_otg_state do_a_host(struct dwc_otg2 *otg)
 	int id = RID_UNKNOWN;
 	unsigned long flags;
 
-	if (otg->usb2_phy.vbus_state == VBUS_DISABLED) {
+	/* If Battery low and connected charger is not ACA-DOCK.
+	 * Then stop trying to start host mode. */
+	if ((otg->usb2_phy.vbus_state == VBUS_DISABLED) &&
+			(otg->charging_cap.chrg_type !=
+			POWER_SUPPLY_CHARGER_TYPE_ACA_DOCK)) {
 		otg_uevent_trigger(&otg->usb2_phy);
 		return DWC_STATE_B_IDLE;
 	}
@@ -650,8 +654,7 @@ stay_host:
 	}
 
 	/* Higher priority first */
-	if (otg_events & OEVT_A_DEV_SESS_END_DET_EVNT ||
-			user_events & USER_A_BUS_DROP) {
+	if (otg_events & OEVT_A_DEV_SESS_END_DET_EVNT) {
 		otg_dbg(otg, "OEVT_A_DEV_SESS_END_DET_EVNT\n");
 
 		/* ACA-Dock plug out */
@@ -662,6 +665,19 @@ stay_host:
 		else
 			dwc_otg_enable_vbus(otg, 0);
 
+		stop_host(otg);
+		return DWC_STATE_B_IDLE;
+	}
+
+	if (user_events & USER_A_BUS_DROP) {
+		/* Due to big consume by DUT, even ACA-Dock connected,
+		 * the battery capability still maybe decrease. For this
+		 * case, still save host mode. Because DUT haven't drive VBus.*/
+		if (otg->charging_cap.chrg_type ==
+				POWER_SUPPLY_CHARGER_TYPE_ACA_DOCK)
+			goto stay_host;
+
+		dwc_otg_enable_vbus(otg, 0);
 		stop_host(otg);
 		return DWC_STATE_B_IDLE;
 	}
