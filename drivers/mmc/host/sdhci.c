1f1a0d984d02e809743aedfc160dbb8e9795b71b
yisun1 <yi.y.sun@intel.com>
2013-11-12 18:51:47 +0800

    mmc: Fix unexpected interrupt issue caused by R1B
    BZ: 150133
    
    Upstream codes assume cmd interrupt comes
    before data interrupt. But this is not true
    for r1b cmd which causes unexpected interrupt
    appears.
    
    Change-Id: I4c091548fed180ee7201ffdb7fdc9985ff94360b
    Signed-off-by: yisun1 <yi.y.sun@intel.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1060,6 +1060,7 @@ static void sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 	mod_timer(&host->timer, jiffies + 10 * HZ);
 
 	host->cmd = cmd;
+	host->r1b_busy_end = 0;
 
 	sdhci_prepare_data(host, cmd);
 
@@ -2700,7 +2701,10 @@ static void sdhci_cmd_irq(struct sdhci_host *host, u32 intmask)
 			DBG("Cannot wait for busy signal when also "
 				"doing a data transfer");
 		else if (!(host->quirks & SDHCI_QUIRK_NO_BUSY_IRQ))
-			return;
+			if (!host->r1b_busy_end) {
+				host->r1b_busy_end = 1;
+				return;
+			}
 
 		/* The controller does not support the end-of-busy IRQ,
 		 * fall through and take the SDHCI_INT_RESPONSE */
@@ -2761,7 +2765,10 @@ static void sdhci_data_irq(struct sdhci_host *host, u32 intmask)
 		 */
 		if (host->cmd && (host->cmd->flags & MMC_RSP_BUSY)) {
 			if (intmask & SDHCI_INT_DATA_END) {
-				sdhci_finish_command(host);
+				if (host->r1b_busy_end)
+					sdhci_finish_command(host);
+				else
+					host->r1b_busy_end = 1;
 				return;
 			} else if (intmask & SDHCI_INT_DATA_TIMEOUT) {
 				pr_err("%s: Got data interrupt 0x%08x for busy cmd %d\n",
@@ -3049,6 +3056,7 @@ static void sdhci_panic_send_cmd(struct sdhci_host *host,
 	}
 
 	host->cmd = cmd;
+	host->r1b_busy_end = 0;
 
 	/*
 	 * set the data timeout register to be max value
@@ -3244,7 +3252,10 @@ static void sdhci_panic_cmd_irq(struct sdhci_host *host, u32 intmask)
 		if (host->cmd->data)
 			pr_debug("Cannot wait for busy signal when also doing a data transfer\n");
 		else if (!(host->quirks & SDHCI_QUIRK_NO_BUSY_IRQ))
-			return;
+			if (!host->r1b_busy_end) {
+				host->r1b_busy_end = 1;
+				return;
+			}
 	}
 
 	if (intmask & SDHCI_INT_RESPONSE)
@@ -3270,7 +3281,10 @@ static void sdhci_panic_data_irq(struct sdhci_host *host, u32 intmask)
 		 */
 		if (host->cmd && (host->cmd->flags & MMC_RSP_BUSY)) {
 			if (intmask & SDHCI_INT_DATA_END) {
-				sdhci_panic_finish_command(host);
+				if (host->r1b_busy_end)
+					sdhci_panic_finish_command(host);
+				else
+					host->r1b_busy_end = 1;
 				return;
 			}
 		}
