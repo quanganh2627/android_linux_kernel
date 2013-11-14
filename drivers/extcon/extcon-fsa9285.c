f60aa45072a8e7d08f440b0fe145ac49342cd405
Han Zhen <zhen.han@intel.com>
2013-11-14 16:49:48 +0800

    ifsa9285: clear the fsa interrupt quickly to avoid the next interrupt lost
    BZ: 143919
    
    Currently, sometimes, the system fails in detecting USB insertion,
    which will result in adb or charger function failure. Checked found that,
    in the failure case, fsa will report two interrupts (Vbus_valid and BC1.2_Complete)
    at one time. However, the device_type was not reported in correspond register(0x0A).
    
    This patch fixs this issue by:
    1. Advancing code "clear interrupt" before "fsa9285_detect_dev". Fsa9285 interrupt is level type.
       We need clear interrupt as soon as possibile to not lost following interrupt, because
       fsa9285_detect_dev() needs quite a long time.
    
    2. Removing codes "masking/unmasking fsa9285 interrupt". In kernel, irq has been disabled
       before irq_handler. So wee do not need to mask/unmask fsa9285 interrupt again in chip side.
    
    3. Modify fsa9285_irq type to IRQF_TRIGGER_LOW. According Fsa9285 spec, its INTB
       is "level low active". Currently, fsa9285_irq type is set as falling edge.
    
    4. Disable/enable VBUS INT when enabling VBUS. These codes are added for the FFRD10/FFRD8,
       which are different from BYT_RC. Without them, the FFRD10/FFRD8 could not work when
       connecting USB-OTG mouse. However, BTY_CR works well.
    
    Change-Id: I43caec3e68598c948a9787616a1f06faaa56aea1
    Signed-off-by: Han Zhen <zhen.han@intel.com>
    Signed-off-by: Li Zhuangzhi <zhuangzhi.li@intel.com>

--- drivers/extcon/extcon-fsa9285.c
+++ drivers/extcon/extcon-fsa9285.c
@@ -225,7 +225,7 @@ static int fsa9285_detect_dev(struct fsa9285_chip *chip)
 	static bool notify_otg, notify_charger;
 	static char *cable;
 	static struct power_supply_cable_props cable_props;
-	int stat, devtype, ohm_code, cntl, ret;
+	int stat, devtype, ohm_code, cntl, intmask, ret;
 	u8 w_man_sw, w_man_chg_cntl;
 	bool discon_evt = false, drive_vbus = false;
 	int vbus_mask = 0;
@@ -351,9 +351,32 @@ static int fsa9285_detect_dev(struct fsa9285_chip *chip)
 	}
 
 	/* VBUS control */
-	if (drive_vbus && !chip->a_bus_drop)
+	if (drive_vbus && !chip->a_bus_drop) {
+		intmask = fsa9285_read_reg(client, FSA9285_REG_INTR_MASK);
+		if (intmask < 0)
+			goto dev_det_i2c_failed;
+
+		/* disable VBUS interrupt */
+		ret = fsa9285_write_reg(client, FSA9285_REG_INTR_MASK,
+						intmask | INTR_VBUS_CHANGE);
+		if (ret < 0)
+			goto dev_det_i2c_failed;
+
 		ret = chip->pdata->enable_vbus();
-	else
+
+		/* clear interrupt */
+		ret = fsa9285_read_reg(client, FSA9285_REG_INTR);
+		if (ret < 0)
+			dev_err(&chip->client->dev,
+				"i2c read failed:%d\n", ret);
+
+		/* enable VBUS interrupt */
+		ret = fsa9285_write_reg(client, FSA9285_REG_INTR_MASK,
+						intmask);
+		if (ret < 0)
+			goto dev_det_i2c_failed;
+
+	} else
 		ret = chip->pdata->disable_vbus();
 	if (ret < 0)
 		dev_warn(&chip->client->dev,
@@ -434,31 +457,20 @@ static irqreturn_t fsa9285_irq_handler(int irq, void *data)
 
 	pm_runtime_get_sync(&chip->client->dev);
 
-	/* mask the interrupts */
-	ret = fsa9285_write_reg(client, FSA9285_REG_CTRL,
-					chip->cntl | CTRL_INT_MASK);
-	if (ret < 0)
+	/* clear interrupt */
+	ret = fsa9285_read_reg(client, FSA9285_REG_INTR);
+	if (ret <= 0) {
+		dev_info(&client->dev, "invalid intr:%x\n", ret);
 		goto isr_ret;
-	/*Add delay as a WA for device responce time*/
-	mdelay(10);
+	} else
+		dev_info(&client->dev, "clear intr:%x\n", ret);
 
+	mdelay(10);
 	/* device detection */
 	ret = fsa9285_detect_dev(chip);
 	if (ret < 0)
-		goto isr_ret;
-
-	/* clear interrupt */
-	ret = fsa9285_read_reg(client, FSA9285_REG_INTR);
-	if (ret < 0)
-		dev_err(&chip->client->dev, "i2c read failed:%d\n", ret);
-	else
-		dev_info(&client->dev, "intr:%x\n", ret);
-
-	/* unmask the interrupts */
-	ret = fsa9285_write_reg(client, FSA9285_REG_CTRL,
-					chip->cntl & ~CTRL_INT_MASK);
-	/*Add delay as a WA for device responce time*/
-	mdelay(10);
+		dev_err(&chip->client->dev,
+				"fsa9285 detecting devices failed:%d\n", ret);
 isr_ret:
 	pm_runtime_put_sync(&chip->client->dev);
 	return IRQ_HANDLED;
@@ -528,7 +540,7 @@ static int fsa9285_irq_init(struct fsa9285_chip *chip)
 	if (client->irq) {
 		ret = request_threaded_irq(client->irq, NULL,
 				fsa9285_irq_handler,
-				IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
+				IRQF_TRIGGER_LOW | IRQF_ONESHOT,
 				"fsa9285", chip);
 		if (ret) {
 			dev_err(&client->dev, "failed to reqeust IRQ\n");
