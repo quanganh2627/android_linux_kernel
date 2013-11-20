2bb5ffb09651eee4237684af7ed6f1b4c26ada8a
Nivedha Krishnakumar <nivedha.krishnakumar@intel.com>
2013-11-20 08:45:32 +0530

    intel_idle.c: Extending Idle driver support for Anniedale Processor
    BZ: 131730
    
    This commit extends the Idle driver support for Anniedale Processor
    by including the CPUID and assigning the cpuidle state table to it.
    
    Change-Id: I43dd1bde2673394985581e0af93d68b3e6e825e2
    Signed-off-by: Nivedha Krishnakumar <nivedha.krishnakumar@intel.com>
    Reviewed-on: http://android.intel.com:8080/146085
    Reviewed-by: Rudramuni, Vishwesh M <vishwesh.m.rudramuni@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Odeberg, Hans <hans.odeberg@intel.com>
    Tested-by: Cuesta, Fernand <fernand.cuesta@intel.com>
    Reviewed-by: Jin, AlbertX <albertx.jin@intel.com>
    Tested-by: Jin, AlbertX <albertx.jin@intel.com>

--- drivers/idle/intel_idle.c
+++ drivers/idle/intel_idle.c
@@ -481,6 +481,42 @@ static struct cpuidle_state mrfld_cstates[CPUIDLE_STATE_MAX] = {
 #define mrfld_cstates atom_cstates
 #endif
 
+static struct cpuidle_state moorfld_cstates[CPUIDLE_STATE_MAX] = {
+	{ /* MWAIT C1 */
+		.name = "C1-ATM",
+		.desc = "MWAIT 0x00",
+		.flags = MWAIT2flg(0x00) | CPUIDLE_FLAG_TIME_VALID,
+		.exit_latency = 1,
+		.target_residency = 4,
+		.enter = &intel_idle },
+	{ /* MWAIT C6 */
+		.name = "C6-ATM",
+		.desc = "MWAIT 0x52",
+		.flags = MWAIT2flg(0x52) | CPUIDLE_FLAG_TIME_VALID
+					 | CPUIDLE_FLAG_TLB_FLUSHED,
+		.exit_latency = 140,
+		.target_residency = 560,
+		.enter = &intel_idle },
+	{ /* MWAIT C7-S0i1 */
+		.name = "S0i1-ATM",
+		.desc = "MWAIT 0x60",
+		.flags = MWAIT2flg(0x60) | CPUIDLE_FLAG_TIME_VALID
+					 | CPUIDLE_FLAG_TLB_FLUSHED,
+		.exit_latency = 1200,
+		.target_residency = 4000,
+		.enter = &intel_idle },
+	{ /* MWAIT C9-S0i3 */
+		.name = "S0i3-ATM",
+		.desc = "MWAIT 0x64",
+		.flags = MWAIT2flg(0x64) | CPUIDLE_FLAG_TIME_VALID
+					 | CPUIDLE_FLAG_TLB_FLUSHED,
+		.exit_latency = 10000,
+		.target_residency = 20000,
+		.enter = &intel_idle },
+	{
+		.enter = NULL }
+};
+
 #if defined(CONFIG_REMOVEME_INTEL_ATOM_MDFLD_POWER) || \
 	defined(CONFIG_REMOVEME_INTEL_ATOM_CLV_POWER)
 
@@ -879,6 +915,10 @@ static const struct idle_cpu idle_cpu_vlv = {
 	.state_table = vlv_cstates,
 };
 
+static const struct idle_cpu idle_cpu_moorfld = {
+	.state_table = moorfld_cstates,
+};
+
 static const struct idle_cpu idle_cpu_chv = {
 	.state_table = chv_cstates,
 };
@@ -909,6 +949,7 @@ static const struct x86_cpu_id intel_idle_ids[] = {
 	ICPU(0x27, idle_cpu_mfld),
 	ICPU(0x35, idle_cpu_mfld),
 	ICPU(0x4a, idle_cpu_mrfld),	/* Tangier SoC */
+	ICPU(0x5a, idle_cpu_moorfld),	/* Anniedale SoC */
 	{}
 };
 MODULE_DEVICE_TABLE(x86cpu, intel_idle_ids);
