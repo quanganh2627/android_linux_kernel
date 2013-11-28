c6c4dc98f49b00630f6e11c3c6424b112cc83e57
Yunpeng Gao <yunpeng.gao@intel.com>
2013-11-28 11:48:00 +0800

    [PORT FROM K34] mmc: busy wait during SD 1.8v switch to avoid ACG issue
    BZ: 104414
    
    Some SD card need X (device dependent) clock cycles
    to drive data lines DAT[3:0] from 0000b to 1111b.
    Since the time varies between SD cards, then after
    host controller enable SD clock and wait for 1ms,
    the ACG (based on the threshold set) may kick in
    and thus the SD clock will be stopped and thus the
    SD card can not drive the DAT line any more.
    
    This patch just replace the 1ms delay with busy_wait.
    It will repeat read a SDHC host controller register
    every 4us to make sure the ACG will not be triggered
    during the 1ms delay.
    
    Change-Id: I65b3aa2ffb308057ee8010954dc57b5d8a20d054
    Orig-Change-Id: I45508bcd4db786a8b92d0ca971f4109662afd28e
    Signed-off-by: Yunpeng Gao <yunpeng.gao@intel.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -203,8 +203,10 @@ static void sdhci_disable_card_detection(struct sdhci_host *host)
 	sdhci_set_card_detection(host, false);
 }
 
-static void sdhci_busy_wait(struct sdhci_host *host, u32 delay)
+static void sdhci_busy_wait(struct mmc_host *mmc, u32 delay)
 {
+	struct sdhci_host *host = mmc_priv(mmc);
+
 	/* totally 'delay' us, each loop 4us */
 	u32 loop = delay / 4;
 	while (loop) {
@@ -1014,7 +1016,7 @@ static void sdhci_finish_data(struct sdhci_host *host)
 		 */
 		if (data->error) {
 			if (host->quirks2 & SDHCI_QUIRK2_WAIT_FOR_IDLE)
-				sdhci_busy_wait(host, 1000);
+				sdhci_busy_wait(host->mmc, 1000);
 			sdhci_reset(host, SDHCI_RESET_CMD);
 			sdhci_reset(host, SDHCI_RESET_DATA);
 		}
@@ -2472,7 +2474,7 @@ static void sdhci_card_event(struct mmc_host *mmc)
 			mmc_hostname(host->mmc));
 
 		if (host->quirks2 & SDHCI_QUIRK2_WAIT_FOR_IDLE)
-			sdhci_busy_wait(host, 1000);
+			sdhci_busy_wait(mmc, 1000);
 		sdhci_reset(host, SDHCI_RESET_CMD);
 		sdhci_reset(host, SDHCI_RESET_DATA);
 
@@ -2513,6 +2515,7 @@ static const struct mmc_host_ops sdhci_ops = {
 	.card_busy	= sdhci_card_busy,
 	.set_dev_power = sdhci_set_dev_power,
 	.init_card = sdhci_init_card,
+	.busy_wait	= sdhci_busy_wait,
 };
 
 /*****************************************************************************\
@@ -2574,7 +2577,7 @@ static void sdhci_tasklet_finish(unsigned long param)
 		/* Spec says we should do both at the same time, but Ricoh
 		   controllers do not like that. */
 		if (host->quirks2 & SDHCI_QUIRK2_WAIT_FOR_IDLE)
-			sdhci_busy_wait(host, 1000);
+			sdhci_busy_wait(host->mmc, 1000);
 		sdhci_reset(host, SDHCI_RESET_CMD);
 		sdhci_reset(host, SDHCI_RESET_DATA);
 	}
