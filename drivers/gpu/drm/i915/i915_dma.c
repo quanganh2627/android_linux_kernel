2c8ac3289a014e40a888bd07a3064bfc431d97e7
arsharma <ankitprasad.r.sharma@intel.com>
2013-12-04 13:49:16 +0530

    drm/i915: Command parsing hints in open source
    BZ: 156466
    
    Comment in the file i915_dma.c hints a place to poke HW to find
    HW security issues. And may be interpreted as knowing state of
    how hard the security filter is. Revising the comment.
    
    Change-Id: Ifc3158c0b9d08b08a9175e84b827876359b1cbcc
    Signed-off-by: arsharma <ankitprasad.r.sharma@intel.com>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -283,8 +283,7 @@ static int i915_dma_init(struct drm_device *dev, void *data,
 	return retcode;
 }
 
-/* Implement basically the same security restrictions as hardware does
- * for MI_BATCH_NON_SECURE.  These can be made stricter at any time.
+/* Implement check to validate batch buffer.
  *
  * Most of the calculations below involve calculating the size of a
  * particular instruction.  It's important to get the size right as
