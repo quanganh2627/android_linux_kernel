0cbb25cb0e978d7ad9a08e553b8640e18c17b35a
Deepak S <deepak.s@intel.com>
2013-12-06 15:24:40 +0530

    drm/i915: Drop forcewake w/a for missed interrupts/seqno on Sandybridge
    BZ: 160545
    
    [Port from 3.13.0]
    commit 67e5871be82fec1451801d448b51d9a403d1ffac
    Author: Chris Wilson <chris@chris-wilson.co.uk>
    Date:   Fri Nov 22 20:35:24 2013 +0000
    
    I believe, and an evening of i-g-t, that our original workaround for the
    missed interrupts on Sandybridge, that of holding forcewake whilst we
    wait for an interrupts, is no longer required. This leaves us dependent
    on the second workaround of forcing an UC read of the ACTHD before
    reading back the seqno from the snooped HWS. Dropping the forcewake
    should allow us to conserve a little power, not much as the GPU is meant
    to be busy whilst we wait for it!
    
    Change-Id: I938835b9df8b5453ff3a9c5debbec654c5f9076f
    Signed-off-by: Deepak S <deepak.s@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_ringbuffer.c
+++ drivers/gpu/drm/i915/intel_ringbuffer.c
@@ -1064,15 +1064,6 @@ gen6_ring_get_irq(struct intel_ring_buffer *ring)
 	if (!dev->irq_enabled)
 	       return false;
 
-	/* It looks like we need to prevent the gt from suspending while waiting
-	 * for an notify irq, otherwise irqs seem to get lost on at least the
-	 * blt/bsd rings on ivb. */
-	/* Wake up only the relevant engine based on ring type */
-	if (ring->id == RCS)
-		gen6_gt_force_wake_get(dev_priv, FORCEWAKE_RENDER);
-	else
-		gen6_gt_force_wake_get(dev_priv, FORCEWAKE_MEDIA);
-
 	spin_lock_irqsave(&dev_priv->irq_lock, flags);
 	if (ring->irq_refcount++ == 0) {
 		imr = I915_READ_IMR(ring);
@@ -1102,11 +1093,6 @@ gen6_ring_put_irq(struct intel_ring_buffer *ring)
 	}
 	spin_unlock_irqrestore(&dev_priv->irq_lock, flags);
 
-	/* Put down only the relevant engine based on ring type */
-	if (ring->id == RCS)
-		gen6_gt_force_wake_put(dev_priv, FORCEWAKE_RENDER);
-	else
-		gen6_gt_force_wake_put(dev_priv, FORCEWAKE_MEDIA);
 }
 
 static bool
