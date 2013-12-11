59115642dd4bab9409ab306ce0643dca66197ed2
Gabriel Touzeau <gabrielx.touzeau@intel.com>
2013-12-11 09:01:44 +0100

    Android alarm: implement a new Android wake up from off alarm
    BZ: 144574
    
    This patch implements a new Android alarm type that can be setup from
    user space and that will have the capability to wake up phone from off.
    
    Linux kernel alarms must support ALARM_REALTIME_OFF type to be able
    to use this patch. This new alarm type is added not to break any
    current alarm functionnality. This alarm is copied by linux kernel
    to RTC block when phone goes off, and will end up in PMIC which can
    wake up phone from mechanical off.
    
    This patch comes as part of the overall wakeup from OFF feature
    supported on Intel mobile platforms.
    
    Change-Id: Ieec7e3073a4c26bcbe28d31c0be957c131985270
    Signed-off-by: Gabriel Touzeau <gabrielx.touzeau@intel.com>

--- drivers/staging/android/alarm-dev.c
+++ drivers/staging/android/alarm-dev.c
@@ -44,7 +44,8 @@ do {									\
 
 #define ANDROID_ALARM_WAKEUP_MASK ( \
 	ANDROID_ALARM_RTC_WAKEUP_MASK | \
-	ANDROID_ALARM_ELAPSED_REALTIME_WAKEUP_MASK)
+	ANDROID_ALARM_ELAPSED_REALTIME_WAKEUP_MASK | \
+	ANDROID_ALARM_POWER_OFF_WAKEUP_MASK)
 
 static int alarm_opened;
 static DEFINE_SPINLOCK(alarm_slock);
@@ -68,7 +69,8 @@ static struct devalarm alarms[ANDROID_ALARM_TYPE_COUNT];
 static int is_wakeup(enum android_alarm_type type)
 {
 	return (type == ANDROID_ALARM_RTC_WAKEUP ||
-		type == ANDROID_ALARM_ELAPSED_REALTIME_WAKEUP);
+		type == ANDROID_ALARM_ELAPSED_REALTIME_WAKEUP ||
+		type == ANDROID_ALARM_POWER_OFF_WAKEUP);
 }
 
 
@@ -185,6 +187,7 @@ static int alarm_get_time(enum android_alarm_type alarm_type,
 	switch (alarm_type) {
 	case ANDROID_ALARM_RTC_WAKEUP:
 	case ANDROID_ALARM_RTC:
+	case ANDROID_ALARM_POWER_OFF_WAKEUP:
 		getnstimeofday(ts);
 		break;
 	case ANDROID_ALARM_ELAPSED_REALTIME_WAKEUP:
@@ -422,6 +425,8 @@ static int __init alarm_dev_init(void)
 
 	alarm_init(&alarms[ANDROID_ALARM_RTC_WAKEUP].u.alrm,
 			ALARM_REALTIME, devalarm_alarmhandler);
+	alarm_init(&alarms[ANDROID_ALARM_POWER_OFF_WAKEUP].u.alrm,
+			ALARM_REALTIME_OFF, devalarm_alarmhandler);
 	hrtimer_init(&alarms[ANDROID_ALARM_RTC].u.hrt,
 			CLOCK_REALTIME, HRTIMER_MODE_ABS);
 	alarm_init(&alarms[ANDROID_ALARM_ELAPSED_REALTIME_WAKEUP].u.alrm,
