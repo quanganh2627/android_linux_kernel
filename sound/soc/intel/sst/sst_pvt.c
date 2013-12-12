6c1f46505d041c42e927ec45f27eb4151021a582
Vinod Koul <vinod.koul@intel.com>
2013-12-12 15:15:45 +0530

    Audio: sst: Recovery can be triggered only when the FW is already downloaded
    BZ: 156967
    
    When FW is downloading the stream recovery can't be done.
    
    Change-Id: I8e8375bbef9101e2fd6d09c2667d80bb73251913
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Samreen Nilofer <samreen.nilofer@intel.com>

--- sound/soc/intel/sst/sst_pvt.c
+++ sound/soc/intel/sst/sst_pvt.c
@@ -365,14 +365,17 @@ int sst_wait_timeout(struct intel_sst_drv *sst_drv_ctx, struct sst_block *block)
 		retval = -block->ret_code;
 	} else {
 		block->on = false;
-		pr_err("sst: Wait timed-out condition:%#x, msg_id:%#x\n",
-				block->condition, block->msg_id);
+		pr_err("sst: Wait timed-out condition:%#x, msg_id:%#x fw_state %#x\n",
+				block->condition, block->msg_id, sst_drv_ctx->sst_state);
 
-		sst_do_recovery(sst_drv_ctx);
-		/* settign firmware state as uninit so that the
-		firmware will get redownloaded on next request
-		this is because firmare not responding for 5 sec
-		is equalant to some unrecoverable error of FW */
+		if (sst_drv_ctx->sst_state == SST_FW_LOADED) {
+			pr_err("Can't recover as timedout while downloading the FW\n");
+			pr_err("reseting fw state to unint...\n");
+			sst_drv_ctx->sst_state = SST_UN_INIT;
+		} else {
+			sst_do_recovery(sst_drv_ctx);
+		}
+
 		retval = -EBUSY;
 	}
 	return retval;
