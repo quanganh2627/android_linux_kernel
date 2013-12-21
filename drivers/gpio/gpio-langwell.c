d5387bbc84d5e5e5a1cb30bc93f741ea5b295d1b
Ivan Gorinov <ivan.gorinov@intel.com>
2013-12-20 19:54:45 -0800

    Roll back GPIO driver patch that changes FLIS settings
    BZ: 160538
    
    This change rolls back patch 141181.
    
    Change-Id: Id349b5e9d574ee184f78d95fc48a170190a0e8d4
    Signed-off-by: Ivan Gorinov <ivan.gorinov@intel.com>

--- drivers/gpio/gpio-langwell.c
+++ drivers/gpio/gpio-langwell.c
@@ -412,49 +412,9 @@ static int lnw_gpio_get(struct gpio_chip *chip, unsigned offset)
 	return readl(gplr) & BIT(offset % 32);
 }
 
-#define PULLUP_ENABLE	(1 << 8)
-#define PULLDOWN_ENABLE	(1 << 9)
-#define PUPD_VAL_2K	(0 << 4)
-#define PUPD_VAL_20K	(1 << 4)
-#define PUPD_VAL_50K	(2 << 4)
-#define PUPD_VAL_910	(3 << 4)
-
-static int lnw_gpio_set_pull(struct gpio_chip *chip, unsigned gpio, int value)
-{
-	u32 flis_offset, flis_value;
-	struct lnw_gpio *lnw = to_lnw_priv(chip);
-	unsigned long flags;
-
-	if (lnw->type != TANGIER_GPIO)
-		return 0;
-
-	flis_offset = lnw->get_flis_offset(gpio);
-	if (WARN(flis_offset == -EINVAL, "invalid pin %d\n", gpio))
-		return -EINVAL;
-	if (is_merr_i2c_flis(flis_offset))
-		return 0;
-
-	spin_lock_irqsave(&lnw->lock, flags);
-	flis_value = get_flis_value(flis_offset);
-	if (value) {
-		flis_value |= PULLUP_ENABLE;
-		flis_value &= ~PULLDOWN_ENABLE;
-	} else {
-		flis_value |= PULLDOWN_ENABLE;
-		flis_value &= ~PULLUP_ENABLE;
-	}
-	flis_value |= PUPD_VAL_50K;
-	set_flis_value(flis_value, flis_offset);
-	spin_unlock_irqrestore(&lnw->lock, flags);
-
-	return 0;
-}
-
 static void lnw_gpio_set(struct gpio_chip *chip, unsigned offset, int value)
 {
 	void __iomem *gpsr, *gpcr;
-
-	lnw_gpio_set_pull(chip, offset, value);
 
 	if (value) {
 		gpsr = gpio_reg(chip, offset, GPSR);
