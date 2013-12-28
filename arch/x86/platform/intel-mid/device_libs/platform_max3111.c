aaf07ad0dbfdd83d4ec918cb7072611e4b226f19
Bin Gao <bin.gao@intel.com>
2013-12-28 15:53:52 -0800

    max3111: add platform data for mofd
    BZ: 161610
    
    add platform data for mofd.
    
    Change-Id: I10f898697fb1255794357fbba1917699420d3f1b
    Signed-off-by: Bin Gao <bin.gao@intel.com>
    Signed-off-by: Renganathan, Prabu <prabu.renganathan@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_max3111.c
+++ arch/x86/platform/intel-mid/device_libs/platform_max3111.c
@@ -40,7 +40,10 @@ void __init *max3111_platform_data(void *info)
 		return &max3110_pdata;
 	}
 
-	if (INTEL_MID_BOARD(1, PHONE, MRFL)) {
+	if (INTEL_MID_BOARD(1, PHONE, MOFD)) {
+		spi_info->controller_data = &chip;
+		spi_info->bus_num = FORCE_SPI_BUS_NUM;
+	} else if (INTEL_MID_BOARD(1, PHONE, MRFL)) {
 		spi_info->controller_data = &chip;
 		spi_info->bus_num = FORCE_SPI_BUS_NUM;
 
