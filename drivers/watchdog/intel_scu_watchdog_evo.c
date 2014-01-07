47f99ef07a130ab4d784d8c14f84c988d34b6df8
Gaelle Nassiet <gaellex.nassiet@intel.com>
2014-01-07 17:44:48 +0100

    Enable EVO watchdog for Anniedale
    BZ: 162713
    
    Update driver to support Anniedale.
    
    Change-Id: I1b30f6667f583b5d3dfa7c60035ebffd6be2cc9f
    Signed-off-by: Gaelle Nassiet <gaellex.nassiet@intel.com>

--- drivers/watchdog/intel_scu_watchdog_evo.c
+++ drivers/watchdog/intel_scu_watchdog_evo.c
@@ -1424,7 +1424,8 @@ static struct rpmsg_driver watchdog_rpmsg = {
 
 static int __init watchdog_rpmsg_init(void)
 {
-	if (intel_mid_identify_cpu() == INTEL_MID_CPU_CHIP_TANGIER)
+	if ((intel_mid_identify_cpu() == INTEL_MID_CPU_CHIP_TANGIER) ||
+		(intel_mid_identify_cpu() == INTEL_MID_CPU_CHIP_ANNIEDALE))
 		return register_rpmsg_driver(&watchdog_rpmsg);
 	else {
 		pr_err("%s: watchdog driver: bad platform\n", __func__);
