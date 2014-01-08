1fb3ad195f99a7c315cbf43b69bf97fcf35dd4ef
Harish Maiya <harish.maiya@intel.com>
2014-01-08 09:24:26 -0800

    PCI: New API to read/write PCI registers by building raw command
    BZ: 163420
    
    Following new intelmid API was impelemented by Bin Gao based on request
    from VISA driver team. This will enable using common API to access the
    PCI read/write register resources. It was causing data corruption if
    different tools are using own mechanism.
    
    intel_mid_msgbus_read32_raw_ext
    intel_mid_msgbus_write32_raw_ext
    
    Also replace leading white space with tab to somply with kernel coding
    standard.
    
    Change-Id: I6e90456f849989dc6621ebf9a7c52197f33c6c2d
    Signed-off-by: Vishnu Naikawadi <vishnu.naikawadi@intel.com>

--- arch/x86/platform/intel-mid/intel_mid_pcihelpers.c
+++ arch/x86/platform/intel-mid/intel_mid_pcihelpers.c
@@ -11,96 +11,128 @@ static struct pci_dev *pci_root;
 
 static int intel_mid_msgbus_init(void)
 {
-        pci_root = pci_get_bus_and_slot(0, PCI_DEVFN(0, 0));
-        if (!pci_root) {
-                printk(KERN_ALERT "%s: Error: msgbus PCI handle NULL",
-                        __func__);
-                return -ENODEV;
-        }
-        return 0;
+	pci_root = pci_get_bus_and_slot(0, PCI_DEVFN(0, 0));
+	if (!pci_root) {
+		pr_err("%s: Error: msgbus PCI handle NULL\n", __func__);
+		return -ENODEV;
+	}
+	return 0;
 }
 fs_initcall(intel_mid_msgbus_init);
 
 u32 intel_mid_msgbus_read32_raw(u32 cmd)
 {
-        unsigned long irq_flags;
-        u32 data;
+	unsigned long irq_flags;
+	u32 data;
 
-        spin_lock_irqsave(&msgbus_lock, irq_flags);
-        pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_REG, cmd);
-        pci_read_config_dword(pci_root, PCI_ROOT_MSGBUS_DATA_REG, &data);
-        spin_unlock_irqrestore(&msgbus_lock, irq_flags);
+	spin_lock_irqsave(&msgbus_lock, irq_flags);
+	pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_REG, cmd);
+	pci_read_config_dword(pci_root, PCI_ROOT_MSGBUS_DATA_REG, &data);
+	spin_unlock_irqrestore(&msgbus_lock, irq_flags);
 
-        return data;
+	return data;
 }
 EXPORT_SYMBOL(intel_mid_msgbus_read32_raw);
 
+/*
+ * GU: this function is only used by the VISA and 'VXD' drivers.
+ */
+u32 intel_mid_msgbus_read32_raw_ext(u32 cmd, u32 cmd_ext)
+{
+	unsigned long irq_flags;
+	u32 data;
+
+	spin_lock_irqsave(&msgbus_lock, irq_flags);
+	pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_EXT_REG, cmd_ext);
+	pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_REG, cmd);
+	pci_read_config_dword(pci_root, PCI_ROOT_MSGBUS_DATA_REG, &data);
+	spin_unlock_irqrestore(&msgbus_lock, irq_flags);
+
+	return data;
+}
+EXPORT_SYMBOL(intel_mid_msgbus_read32_raw_ext);
+
 void intel_mid_msgbus_write32_raw(u32 cmd, u32 data)
 {
-        unsigned long irq_flags;
+	unsigned long irq_flags;
 
-        spin_lock_irqsave(&msgbus_lock, irq_flags);
-        pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_DATA_REG, data);
-        pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_REG, cmd);
-        spin_unlock_irqrestore(&msgbus_lock, irq_flags);
+	spin_lock_irqsave(&msgbus_lock, irq_flags);
+	pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_DATA_REG, data);
+	pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_REG, cmd);
+	spin_unlock_irqrestore(&msgbus_lock, irq_flags);
 }
 EXPORT_SYMBOL(intel_mid_msgbus_write32_raw);
 
+/*
+ * GU: this function is only used by the VISA and 'VXD' drivers.
+ */
+void intel_mid_msgbus_write32_raw_ext(u32 cmd, u32 cmd_ext, u32 data)
+{
+	unsigned long irq_flags;
+
+	spin_lock_irqsave(&msgbus_lock, irq_flags);
+	pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_DATA_REG, data);
+	pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_EXT_REG, cmd_ext);
+	pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_REG, cmd);
+	spin_unlock_irqrestore(&msgbus_lock, irq_flags);
+}
+EXPORT_SYMBOL(intel_mid_msgbus_write32_raw_ext);
+
 u32 intel_mid_msgbus_read32(u8 port, u32 addr)
 {
-        unsigned long irq_flags;
-        u32 data;
-        u32 cmd;
-        u32 cmdext;
+	unsigned long irq_flags;
+	u32 data;
+	u32 cmd;
+	u32 cmdext;
 
-        cmd = (PCI_ROOT_MSGBUS_READ << 24) | (port << 16) |
-                ((addr & 0xff) << 8) | PCI_ROOT_MSGBUS_DWORD_ENABLE;
-        cmdext = addr & 0xffffff00;
+	cmd = (PCI_ROOT_MSGBUS_READ << 24) | (port << 16) |
+		((addr & 0xff) << 8) | PCI_ROOT_MSGBUS_DWORD_ENABLE;
+	cmdext = addr & 0xffffff00;
 
-        spin_lock_irqsave(&msgbus_lock, irq_flags);
+	spin_lock_irqsave(&msgbus_lock, irq_flags);
 
-        if (cmdext) {
-                /* This resets to 0 automatically, no need to write 0 */
-                pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_EXT_REG,
-                        cmdext);
-        }
+	if (cmdext) {
+		/* This resets to 0 automatically, no need to write 0 */
+		pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_EXT_REG,
+					cmdext);
+	}
 
-        pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_REG, cmd);
-        pci_read_config_dword(pci_root, PCI_ROOT_MSGBUS_DATA_REG, &data);
-        spin_unlock_irqrestore(&msgbus_lock, irq_flags);
+	pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_REG, cmd);
+	pci_read_config_dword(pci_root, PCI_ROOT_MSGBUS_DATA_REG, &data);
+	spin_unlock_irqrestore(&msgbus_lock, irq_flags);
 
-        return data;
+	return data;
 }
 
 EXPORT_SYMBOL(intel_mid_msgbus_read32);
 void intel_mid_msgbus_write32(u8 port, u32 addr, u32 data)
 {
-        unsigned long irq_flags;
-        u32 cmd;
-        u32 cmdext;
+	unsigned long irq_flags;
+	u32 cmd;
+	u32 cmdext;
 
-        cmd = (PCI_ROOT_MSGBUS_WRITE << 24) | (port << 16) |
-                ((addr & 0xFF) << 8) | PCI_ROOT_MSGBUS_DWORD_ENABLE;
-        cmdext = addr & 0xffffff00;
+	cmd = (PCI_ROOT_MSGBUS_WRITE << 24) | (port << 16) |
+		((addr & 0xFF) << 8) | PCI_ROOT_MSGBUS_DWORD_ENABLE;
+	cmdext = addr & 0xffffff00;
 
-        spin_lock_irqsave(&msgbus_lock, irq_flags);
-        pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_DATA_REG, data);
+	spin_lock_irqsave(&msgbus_lock, irq_flags);
+	pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_DATA_REG, data);
 
-        if (cmdext) {
-                /* This resets to 0 automatically, no need to write 0 */
-                pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_EXT_REG,
-                        cmdext);
-        }
+	if (cmdext) {
+		/* This resets to 0 automatically, no need to write 0 */
+		pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_EXT_REG,
+					cmdext);
+	}
 
-        pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_REG, cmd);
-        spin_unlock_irqrestore(&msgbus_lock, irq_flags);
+	pci_write_config_dword(pci_root, PCI_ROOT_MSGBUS_CTRL_REG, cmd);
+	spin_unlock_irqrestore(&msgbus_lock, irq_flags);
 }
 EXPORT_SYMBOL(intel_mid_msgbus_write32);
 
 /* called only from where is later then fs_initcall */
 u32 intel_mid_soc_stepping(void)
 {
-        return pci_root->revision;
+	return pci_root->revision;
 }
 EXPORT_SYMBOL(intel_mid_soc_stepping);
 
@@ -113,9 +145,9 @@ static bool is_south_complex_device(struct pci_dev *dev)
 	 * everything else are south complex devices.
 	 */
 	if (((base_class == PCI_BASE_CLASS_MULTIMEDIA) &&
-				(sub_class == ISP_SUB_CLASS)) ||
-		(base_class == PCI_BASE_CLASS_BRIDGE) ||
-		((base_class == PCI_BASE_CLASS_DISPLAY) && !sub_class))
+	     (sub_class == ISP_SUB_CLASS)) ||
+	    (base_class == PCI_BASE_CLASS_BRIDGE) ||
+	    ((base_class == PCI_BASE_CLASS_DISPLAY) && !sub_class))
 		return false;
 	else
 		return true;
