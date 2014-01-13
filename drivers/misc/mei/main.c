7e5d1d4f42e88cdf402ffc0a02a7a7dd68b0637b
Tomas Winkler <tomas.winkler@intel.com>
2014-01-13 11:26:29 +0200

    mei: fix few checkpatch errors
    BZ: 164605
    
    Change-Id: I91d9307af52712cdd56c3d4357f6414a2a2df2d3
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Reviewed-on: https://git-isdc.jer.intel.com/gerrit/1393
    Reviewed-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- drivers/misc/mei/main.c
+++ drivers/misc/mei/main.c
@@ -219,11 +219,13 @@ static ssize_t mei_read(struct file *file, char __user *ubuf,
 		}
 		/* Offset needs to be cleaned for contiguous reads*/
 		if (cb->buf_idx == 0 && *offset > 0) {
-			dev_dbg(&dev->pdev->dev, "idx = 0 offset = %lld\n", (unsigned long long)*offset);
+			dev_dbg(&dev->pdev->dev, "idx = 0 offset = %lld\n",
+					(unsigned long long)*offset);
 			*offset = 0;
 		}
 	} else if (*offset > 0) {
-		dev_dbg(&dev->pdev->dev, "offset = %lld\n", (unsigned long long)*offset);
+		dev_dbg(&dev->pdev->dev, "offset = %lld\n",
+					(unsigned long long)*offset);
 		*offset = 0;
 	}
 
