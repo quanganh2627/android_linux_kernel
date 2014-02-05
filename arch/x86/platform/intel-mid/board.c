4ac64eae4ad6bdd5c8da7e6226df88d315ddca78
Sylvain Bourdos <sylvain.bourdos@intel.com>
2014-02-05 11:21:40 +0100

    telephony: add XMM7260_CONF_8 config for Saltbay 7260 ES2.01
    BZ: 168919
    
    To enable Saltbay PR2.7 ES2.01 or Merrifield with 7260 ES2.01,
    create a telephony configuration that differs from Tangier with
    7260 ES2.
    
    Change-Id: I10260a42fc986ed04cfcb0e2dde5852d27824a4d
    Signed-off-by: Sylvain Bourdos <sylvain.bourdos@intel.com>

--- arch/x86/platform/intel-mid/board.c
+++ arch/x86/platform/intel-mid/board.c
@@ -400,5 +400,7 @@ struct devs_id __initconst device_ids[] = {
 		&sfi_handle_mdm},
 	{"XMM7260_CONF_7", SFI_DEV_TYPE_MDM, 0, &modem_platform_data,
 		&sfi_handle_mdm},
+	{"XMM7260_CONF_8", SFI_DEV_TYPE_MDM, 0, &modem_platform_data,
+		&sfi_handle_mdm},
 	{},
 };
