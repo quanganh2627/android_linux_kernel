6d336e5b084e6cc6a303d5c31287a9b380727f3d
Venkataramana Kotakonda <venkataramana.kotakonda@intel.com>
2014-02-10 15:41:37 +0530

    EM: Change in SDP inlimit based on USB compliance, platform changes.
    BZ: 174994
    
    Platform should draw 500ma always from SDP port if USB
    compliance is not required.
    
    This patch will check the usb compliance override bit from OEM1
    table and draw 500ma always irrespective of enumeration
    and host suspend status if the compliance override bit is set.
    
    Change-Id: I09f7cfe4b8ce18ee38d9224bb5ccf689fd61d143
    Category: device enablement
    Domain: BatteryManagement-Common
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Venkataramana Kotakonda <venkataramana.kotakonda@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_smsc375x.c
+++ arch/x86/platform/intel-mid/device_libs/platform_smsc375x.c
@@ -7,7 +7,9 @@
 #include <asm/intel-mid.h>
 #include <asm/intel_crystalcove_pwrsrc.h>
 #include <asm/intel_bytcr_bcntl.h>
+#include <asm/intel_em_config.h>
 
+#define EM_CONFIG_USB_COMP_MASK (1 << 0)
 static struct smsc375x_pdata smsc_pdata;
 
 /* dummy functions */
@@ -26,9 +28,21 @@ static int smsc375x_is_vbus_online(void *ptr)
 
 void *smsc375x_platform_data(void)
 {
+	int ret = 0;
+	struct em_config_oem1_data oem1_data;
+
+
 	smsc_pdata.enable_vbus = intel_bytcr_boost_enable;
 	smsc_pdata.disable_vbus = intel_bytcr_boost_disable;
 	smsc_pdata.is_vbus_online = crystal_cove_vbus_on_status;
+
+	memset(&oem1_data, 0, sizeof(struct em_config_oem1_data));
+	ret = em_config_get_oem1_data(&oem1_data);
+	/* If usb override  is set, then platform can voilate USB spec */
+	if ((ret > 0) && !(oem1_data.fpo_0 & EM_CONFIG_USB_COMP_MASK))
+		smsc_pdata.charging_compliance_override = false;
+	else
+		smsc_pdata.charging_compliance_override = true;
 
 	return &smsc_pdata;
 }
