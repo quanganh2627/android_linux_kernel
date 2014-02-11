274a3f1e93c3c4605e2f7492d9e187c3605578c8
Jeremy Compostella <jeremy.compostella@intel.com>
2014-02-11 19:40:29 +0100

    drivers: android: binder: Fix binder_node_release broken walk
    BZ: 170969
    
    Fix an issue introduced by commit
    008fa749e0fe5b2fffd20b7fe4891bb80d072c6a ("drivers: android: binder:
    Move the node release code to a separate function") which move and
    rework some code from binder_deferred_release to the
    binder_node_release new function.  The rework introduces an
    unfortunate break of the loop.
    
    Change-Id: Ia5aa0d3dce497bcdbec434bd379f813dcbf59084
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -2954,7 +2954,7 @@ static int binder_node_release(struct binder_node *node, int refs)
 		refs++;
 
 		if (!ref->death)
-			goto out;
+			continue;
 
 		death++;
 
@@ -2967,7 +2967,6 @@ static int binder_node_release(struct binder_node *node, int refs)
 			BUG();
 	}
 
-out:
 	binder_debug(BINDER_DEBUG_DEAD_BINDER,
 		     "node %d now dead, refs %d, death %d\n",
 		     node->debug_id, refs, death);
