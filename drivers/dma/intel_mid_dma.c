c6802dfeb8692df3d0382ec1573d414907691eb2
Processe, Christian <christian.processe@intel.com>
2014-02-27 07:01:50 -0800

    Revert "intel_mid_dma: fix the return value of dma_runtime_idle"
    BZ: 175492
    
    This reverts commit c124539d006aba6b3e3b919bd411ef46c2c417d5.
    This patch revert https://android.intel.com/#/c/158768/
    
    Change-Id: I469d69c5917343b130e9caf6f431a6cdf8de4d90
    Signed-off-by: Processe, Christian <christian.processe@intel.com>

--- drivers/dma/intel_mid_dma.c
+++ drivers/dma/intel_mid_dma.c
@@ -2066,16 +2066,12 @@ static int dma_runtime_idle(struct device *dev)
 {
 	struct middma_device *device = dev_get_drvdata(dev);
 	int i;
-	int err;
 
 	for (i = 0; i < device->max_chan; i++) {
 		if (device->ch[i].in_use)
 			return -EAGAIN;
 	}
-	err = pm_schedule_suspend(dev, 0);
-	if (err)
-		return err;
-	return -EBUSY;
+	return pm_schedule_suspend(dev, 0);;
 }
 
 /******************************************************************************
