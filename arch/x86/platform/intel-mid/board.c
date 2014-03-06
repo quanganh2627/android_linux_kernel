cb08369915924e612b12b1cfdb5566d738a3c91b
Eckhart Koeppen <eckhart.koppen@intel.com>
2014-03-06 13:19:08 +0200

    GFX_Display -- Enabled SDC16x25 and SDC25x16 panels in the kernel
    BZ: 177283
    
    Add the panel to the supported panel list and map the firmware provided
    name to the correct enumeration.
    
    Change-Id: I7a6d2d880ddfbc0cd051d847c4d99367af8aac1c
    Signed-off-by: Eckhart Koeppen <eckhart.koppen@intel.com>
    Signed-off-by: Faxing Lu <faxing.lu@intel.com>

--- arch/x86/platform/intel-mid/board.c
+++ arch/x86/platform/intel-mid/board.c
@@ -265,6 +265,10 @@ struct devs_id __initconst device_ids[] = {
 		&panel_handler},
 	{"PNC_JDI_25x16", SFI_DEV_TYPE_MDM, 0, &no_platform_data,
 		&panel_handler},
+	{"PNC_SDC_16x25", SFI_DEV_TYPE_MDM, 0, &no_platform_data,
+		&panel_handler},
+	{"PNC_SDC_25x16", SFI_DEV_TYPE_MDM, 0, &no_platform_data,
+		&panel_handler},
 
 	{"ctp_lt_wm8994", SFI_DEV_TYPE_IPC, 1, &ctp_audio_platform_data,
 						&ipc_device_handler},
--- include/linux/panel_psb_drv.h
+++ include/linux/panel_psb_drv.h
@@ -50,6 +50,8 @@ enum panel_type {
 	SHARP_10x19_DUAL_CMD,
 	SHARP_25x16_VID,
 	SHARP_25x16_CMD,
+	SDC_16x25_CMD,
+	SDC_25x16_CMD,
 	GCT_DETECT
 };
 
@@ -73,7 +75,9 @@ static struct support_panel_list_t
 		{SHARP_25x16_VID, "PNV_SHARP_25x16"},
 		{SHARP_25x16_CMD, "PNC_SHARP_25x16"},
 		{JDI_25x16_VID, "PNV_JDI_25x16"},
-		{JDI_25x16_CMD, "PNC_JDI_25x16"}
+		{JDI_25x16_CMD, "PNC_JDI_25x16"},
+		{SDC_16x25_CMD, "PNC_SDC_16x25"},
+		{SDC_25x16_CMD, "PNC_SDC_25x16"}
 	};
 
 #define NUM_SUPPORT_PANELS (sizeof( \
