e364d88f23c675846098c6000f34dde0dc6abd5e
Sakari Ailus <sakari.ailus@linux.intel.com>
2014-03-14 15:40:58 +0200

    css2600: isys: Add number of lanes into a header file
    BZ: 183309
    
    Change-Id: I10154bff36304c1e7138288d7783d4a17d031407
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

---
+++ include/media/css2600-isys.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2014 Intel Corporation. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef MEDIA_CSS2600_H
+#define MEDIA_CSS2600_H
+
+#define CSS2600_ISYS_MAX_CSI2_LANES		4
+
+#endif /* MEDIA_CSS2600_H */
