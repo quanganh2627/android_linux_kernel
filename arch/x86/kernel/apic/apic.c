3c78b975be648dc888af2f78bf84c7cbf0e0cf99
wang, biao <biao.wang@intel.com>
2014-03-17 13:43:25 +0800

    APIC: add suspend/resume handling for TSC-deadline mode
    BZ: 180723
    
    Now as we use TSC-deadline mode to generate timer interrupt, we should
    do suspend/resume handling as count-down mode, else AONT will generate
    unexpected interrupt when entering into S3 and cause ipanic_swwdt in
    some corner case.
    
    Change-Id: I21737affff745582e3b27a5020626dcd83a44699
    Signed-off-by: wang, biao <biao.wang@intel.com>
    Signed-off-by: Di Zhang <di.zhang@intel.com>

--- arch/x86/kernel/apic/apic.c
+++ arch/x86/kernel/apic/apic.c
@@ -2235,7 +2235,11 @@ static int lapic_suspend(void)
 	 */
 	if ((intel_mid_identify_cpu() != 0) ||
 			(boot_cpu_data.x86_model == 0x37)) {
-		apic_write(APIC_TMICT, ~0);
+		if (this_cpu_has(X86_FEATURE_TSC_DEADLINE_TIMER))
+			wrmsrl(MSR_IA32_TSC_DEADLINE, 0);
+		else
+			apic_write(APIC_TMICT, ~0);
+
 		return 0;
 	}
 
@@ -2276,6 +2280,7 @@ static void lapic_resume(void)
 	unsigned int l, h;
 	unsigned long flags;
 	int maxlvt;
+	u64 tsc;
 
 	/*
 	 * On intel_mid, the resume flow is a bit different.
@@ -2283,7 +2288,11 @@ static void lapic_resume(void)
 	 */
 	if ((intel_mid_identify_cpu() != 0) ||
 			(boot_cpu_data.x86_model == 0x37)) {
-		apic_write(APIC_TMICT, 10);
+		if (this_cpu_has(X86_FEATURE_TSC_DEADLINE_TIMER)) {
+			rdtscll(tsc);
+			wrmsrl(MSR_IA32_TSC_DEADLINE, tsc + 10);
+		} else
+			apic_write(APIC_TMICT, 10);
 		return;
 	}
 
