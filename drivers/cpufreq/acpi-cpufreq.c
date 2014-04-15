62f988531ef13278c2d4cc86857b6e2c354c9412
Albin B <albin.bala.krishnan@intel.com>
2014-04-16 05:21:59 +0530

    [acpi-cpufreq]: Cap cpu frequency to LFM during boot upon low battery detection
    BZ: 187329
    
    Issue Description:
    The platform's charging OS boot maximum peak current is around 1.8A. In low
    battery case, device shut down during the boot if the cos boot peak current
    consumes beyond 500mA since the SDP charger can supply only 500mA limit.
    
    Solution:
    To reduce the maximum peak current during boot the CPU frequency is capped. In
    case of low battery during boot, OS Loader will pass an additional parameter to
    kernel boot command line argument as "battlow". So if the battery is detected
    critically low during boot then the kernel will cap maximum scaling cpu
    frequency to LFM.
    
    Change-Id: I694977b70769a98b398e35374107a69b500dd22e
    Category: device enablement
    Domain: PM
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Albin B <albin.bala.krishnan@intel.com>

--- drivers/cpufreq/acpi-cpufreq.c
+++ drivers/cpufreq/acpi-cpufreq.c
@@ -80,6 +80,7 @@ static struct acpi_processor_performance __percpu *acpi_perf_data;
 static struct cpufreq_driver acpi_cpufreq_driver;
 
 static unsigned int acpi_pstate_strict;
+static bool battlow;
 static bool boost_enabled, boost_supported;
 static struct msr __percpu *msrs;
 
@@ -678,6 +679,8 @@ static int acpi_cpufreq_blacklist(struct cpuinfo_x86 *c)
 static int acpi_cpufreq_cpu_init(struct cpufreq_policy *policy)
 {
 	unsigned int i;
+	unsigned int freq;
+	unsigned int cpufreqidx = 0;
 	unsigned int valid_states = 0;
 	unsigned int cpu = policy->cpu;
 	struct acpi_cpufreq_data *data;
@@ -816,6 +819,7 @@ static int acpi_cpufreq_cpu_init(struct cpufreq_policy *policy)
 		    perf->states[i].core_frequency * 1000;
 		valid_states++;
 	}
+	cpufreqidx = valid_states - 1;
 	data->freq_table[valid_states].frequency = CPUFREQ_TABLE_END;
 
 	result = cpufreq_frequency_table_cpuinfo(policy, data->freq_table);
@@ -863,6 +867,22 @@ static int acpi_cpufreq_cpu_init(struct cpufreq_policy *policy)
 	 * writing something to the appropriate registers.
 	 */
 	data->resume = 1;
+
+	/**
+	 * Capping the cpu frequency to LFM during boot, if battery is detected
+	 * as critically low.
+	 */
+	if (battlow) {
+		freq = data->freq_table[cpufreqidx].frequency;
+		if (freq != CPUFREQ_ENTRY_INVALID) {
+			pr_info("CPU%u freq is capping to %uKHz\n", cpu, freq);
+			policy->max = freq;
+		} else {
+			pr_err("CPU%u table entry %u is invalid.\n",
+				cpu, cpufreqidx);
+			goto err_freqfree;
+		}
+	}
 
 	return result;
 
@@ -923,6 +943,18 @@ static struct cpufreq_driver acpi_cpufreq_driver = {
 	.owner		= THIS_MODULE,
 	.attr		= acpi_cpufreq_attr,
 };
+
+/**
+ * set_battlow_status - enables "battlow" to cap the max scaling cpu frequency.
+ */
+static int __init set_battlow_status(char *unused)
+{
+	pr_notice("Low Battery detected! Frequency shall be capped.\n");
+	battlow = true;
+	return 0;
+}
+/* Checking "battlow" param on boot, whether battery is critically low or not */
+early_param("battlow", set_battlow_status);
 
 static void __init acpi_cpufreq_boost_init(void)
 {
