c73330e97122b80b845b4c653b45b868bd0a9a22
Dharageswari.R <dharageswari.r@intel.com>
2014-04-22 11:01:47 +0530

    Audio:CHT:Derive codec pointer from dai pointer
    BZ: 189484
    
    In the codec driver, derive codec pointer from dai pointer instead
    of from pcm runtime since pcm runtime pointer can be NULL.
    
    Change-Id: I5937b5fa1dd624b400255afdb123545444f60ffb
    Signed-off-by: Dharageswari.R <dharageswari.r@intel.com>

--- sound/soc/codecs/tlv320aic31xx.c
+++ sound/soc/codecs/tlv320aic31xx.c
@@ -792,8 +792,7 @@ static int aic31xx_hw_params(struct snd_pcm_substream *substream,
 				struct snd_pcm_hw_params *params,
 				struct snd_soc_dai *tmp)
 {
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_codec *codec = rtd->codec;
+	struct snd_soc_codec *codec = tmp->codec;
 	u8 data;
 	dev_dbg(codec->dev, "%s\n", __func__);
 
