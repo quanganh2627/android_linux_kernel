2ee1519085bfdc59d7ece208d5b7ebcaf1f927f9
Durgadoss R <durgadoss.r@intel.com>
2014-04-24 18:50:14 +0530

    EM/THERMAL: Enable intel-rapl for CHV
    BZ: 189565
    
    This patch enables intel-rapl driver for
    CHV platform by adding the CPU model id
    for detection. CPU model id is 0x4C for CHV.
    
    Change-Id: I31ed103fa6dc3f493df7388349f5030707b005b4
    Signed-off-by: Durgadoss R <durgadoss.r@intel.com>

--- drivers/powercap/intel_rapl.c
+++ drivers/powercap/intel_rapl.c
@@ -835,6 +835,7 @@ static int rapl_write_data_raw(struct rapl_domain *rd,
 
 static const struct x86_cpu_id energy_unit_quirk_ids[] = {
 	{ X86_VENDOR_INTEL, 6, 0x37},/* Valleyview */
+	{ X86_VENDOR_INTEL, 6, 0x4C},/* Cherryview */
 	{}
 };
 
@@ -952,6 +953,7 @@ static const struct x86_cpu_id rapl_ids[] = {
 	{ X86_VENDOR_INTEL, 6, 0x37},/* Valleyview */
 	{ X86_VENDOR_INTEL, 6, 0x3a},/* Ivy Bridge */
 	{ X86_VENDOR_INTEL, 6, 0x45},/* Haswell */
+	{ X86_VENDOR_INTEL, 6, 0x4C},/* CherryView */
 	/* TODO: Add more CPU IDs after testing */
 	{}
 };
