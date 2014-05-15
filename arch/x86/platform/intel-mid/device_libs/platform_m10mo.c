6458b4c6a95b9d3bbcca7f93f2eca4b030201074
Kriti Pachhandara <kriti.pachhandara@intel.com>
2014-05-15 10:41:57 +0300

    [PORT FROM R44C] m10mo: Support new firmware for m10mo
    BZ: 197048
    
    Add support for new firmware binaries for m10mo
    
    Change-Id: I831b212bc81cd58faf2c8cef9df082ba4402a5a5
    Signed-off-by: Kriti Pachhandara <kriti.pachhandara@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_m10mo.c
+++ arch/x86/platform/intel-mid/device_libs/platform_m10mo.c
@@ -180,6 +180,8 @@ static int m10mo_csi_configure(struct v4l2_subdev *sd, int flag)
 
 static struct m10mo_fw_id fw_ids[] = {
 	{ "TEST", M10MO_FW_TYPE_1 },
+	{ "S13F0SAHE01", M10MO_FW_TYPE_1 },
+	{ "O13F0SAHE01", M10MO_FW_TYPE_1 },
 	{ NULL, 0},
 };
 
