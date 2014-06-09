b34ef210b4861e1e7168f3801ddaf85ec7eae6d0
Dharageswari.R <dharageswari.r@intel.com>
2014-06-09 14:58:29 +0530

    Audio: Mofd: Configure the hw_params and slot information of be_dailink
    BZ: 201483
    
    This patch configures the hw_params and slot information of be_dailinks.
    It also uses the be_fixup for CODEC<->CODEC especially for ssp1, reads the
    SSP_MUX register of SST to assign appropriate hardware params.
    
    Change-Id: Ic243d12b68c2de5750d6cf55b9392aa572f68523
    Signed-off-by: Dharageswari.R <dharageswari.r@intel.com>

--- sound/soc/intel/board/merr_dpcm_wm8958.c
+++ sound/soc/intel/board/merr_dpcm_wm8958.c
@@ -67,6 +67,38 @@ struct mrfld_8958_mc_private {
 	int spk_gpio;
 };
 
+struct mrfld_slot_info {
+	unsigned int tx_mask;
+	unsigned int rx_mask;
+	int slots;
+	int slot_width;
+};
+
+static const struct snd_soc_pcm_stream mrfld_wm8958_dai_params_ssp1_fm = {
+	.formats = SNDRV_PCM_FMTBIT_S24_LE,
+	.rate_min = SNDRV_PCM_RATE_48000,
+	.rate_max = SNDRV_PCM_RATE_48000,
+	.channels_min = 2,
+	.channels_max = 2,
+};
+
+static const struct snd_soc_pcm_stream mrfld_wm8958_ssp1_bt_nb = {
+	.formats = SNDRV_PCM_FMTBIT_S24_LE,
+	.rate_min = SNDRV_PCM_RATE_8000,
+	.rate_max = SNDRV_PCM_RATE_8000,
+	.channels_min = 2,
+	.channels_max = 2,
+};
+
+static const struct snd_soc_pcm_stream mrfld_wm8958_ssp1_bt_wb = {
+	.formats = SNDRV_PCM_FMTBIT_S24_LE,
+	.rate_min = SNDRV_PCM_RATE_16000,
+	.rate_max = SNDRV_PCM_RATE_16000,
+	.channels_min = 2,
+	.channels_max = 2,
+};
+
+#define SST_BT_FM_MUX_SHIFT	0
 
 /* set_osc_clk0-	enable/disables the osc clock0
  * addr:		address of the register to write to
@@ -229,33 +261,136 @@ static int mrfld_wm8958_compr_set_params(struct snd_compr_stream *cstream)
 	return 0;
 }
 
-static const struct snd_soc_pcm_stream mrfld_wm8958_dai_params = {
+static const struct snd_soc_pcm_stream mrfld_wm8958_dai_params_ssp0 = {
 	.formats = SNDRV_PCM_FMTBIT_S24_LE,
-	.rate_min = 48000,
-	.rate_max = 48000,
+	.rate_min = SNDRV_PCM_RATE_48000,
+	.rate_max = SNDRV_PCM_RATE_48000,
 	.channels_min = 2,
 	.channels_max = 2,
 };
 
+static const struct snd_soc_pcm_stream mrfld_wm8958_dai_params_ssp2 = {
+	.formats = SNDRV_PCM_FMTBIT_S24_LE,
+	.rate_min = SNDRV_PCM_RATE_48000,
+	.rate_max = SNDRV_PCM_RATE_48000,
+	.channels_min = 2,
+	.channels_max = 2,
+};
+
+#define MRFLD_CONFIG_SLOT(slot_tx_mask, slot_rx_mask, num_slot, width)\
+	(struct mrfld_slot_info){ .tx_mask = slot_tx_mask,			\
+				  .rx_mask = slot_rx_mask,			\
+				  .slots = num_slot,				\
+				  .slot_width = width, }
+
+static int merr_set_slot_and_format(struct snd_soc_dai *dai,
+			struct mrfld_slot_info *slot_info, unsigned int fmt)
+{
+	int ret;
+
+	ret = snd_soc_dai_set_tdm_slot(dai, slot_info->tx_mask,
+		slot_info->rx_mask, slot_info->slots, slot_info->slot_width);
+	if (ret < 0) {
+		pr_err("can't set codec pcm format %d\n", ret);
+		return ret;
+	}
+
+	ret = snd_soc_dai_set_fmt(dai, fmt);
+	if (ret < 0) {
+		pr_err("can't set codec DAI configuration %d\n", ret);
+		return ret;
+	}
+	return ret;
+}
+
 static int merr_codec_fixup(struct snd_soc_pcm_runtime *rtd,
 			    struct snd_pcm_hw_params *params)
 {
-	struct snd_interval *rate = hw_param_interval(params,
-			SNDRV_PCM_HW_PARAM_RATE);
-	struct snd_interval *channels = hw_param_interval(params,
-						SNDRV_PCM_HW_PARAM_CHANNELS);
+	int ret = 0;
+	unsigned int fmt;
+	struct mrfld_slot_info *info;
+	struct snd_interval *rate =  hw_param_interval(params, SNDRV_PCM_HW_PARAM_RATE);
+	struct snd_interval *channels = hw_param_interval(params, SNDRV_PCM_HW_PARAM_CHANNELS);
+
+	/* WM8958 slave Mode */
+	fmt =   SND_SOC_DAIFMT_DSP_B | SND_SOC_DAIFMT_IB_NF |
+		SND_SOC_DAIFMT_CBS_CFS;
+	info = &MRFLD_CONFIG_SLOT(0xF, 0xF, 4, SNDRV_PCM_FORMAT_S24_LE);
+	ret = merr_set_slot_and_format(rtd->cpu_dai, info, fmt);
 
 	pr_debug("Invoked %s for dailink %s\n", __func__, rtd->dai_link->name);
 
-	/* The DSP will covert the FE rate to 48k, stereo, 24bits */
-	rate->min = rate->max = 48000;
+	rate->min = rate->max = SNDRV_PCM_RATE_48000;
 	channels->min = channels->max = 2;
 
 	/* set SSP2 to 24-bit */
 	snd_mask_set(&params->masks[SNDRV_PCM_HW_PARAM_FORMAT -
-				    SNDRV_PCM_HW_PARAM_FIRST_MASK],
-				    SNDRV_PCM_FORMAT_S24_LE);
+					SNDRV_PCM_HW_PARAM_FIRST_MASK],
+					SNDRV_PCM_FORMAT_S24_LE);
 	return 0;
+}
+
+#define SST_MUX_REG 25
+#define SST_BT_FM_MUX_SHIFT	0
+#define SST_BT_MODE_SHIFT	2
+static int mrfld_bt_fm_fixup(struct snd_soc_dai_link *dai_link, struct snd_soc_dai *dai)
+{
+	unsigned int fmt;
+	bool is_bt, is_bt_wb;
+	unsigned int mask, reg_val;
+	int ret;
+	struct mrfld_slot_info *info;
+
+	mask = (1 << fls(1)) - 1;
+	reg_val = snd_soc_platform_read(dai->platform, SST_MUX_REG);
+	is_bt = (reg_val >> SST_BT_FM_MUX_SHIFT) & mask;
+	is_bt_wb = (reg_val >> SST_BT_MODE_SHIFT) & mask;
+
+	if (is_bt) {
+		if (is_bt_wb)
+			dai_link->params = &mrfld_wm8958_ssp1_bt_wb;
+		else
+			dai_link->params = &mrfld_wm8958_ssp1_bt_nb;
+
+		fmt = SND_SOC_DAIFMT_IB_NF | SND_SOC_DAIFMT_DSP_A | SND_SOC_DAIFMT_CBS_CFS;
+		info = &MRFLD_CONFIG_SLOT(0x01, 0x01, 1, SNDRV_PCM_FORMAT_S16_LE);
+	} else {
+		fmt = SND_SOC_DAIFMT_IB_NF | SND_SOC_DAIFMT_LEFT_J | SND_SOC_DAIFMT_CBS_CFS;
+		dai_link->params = &mrfld_wm8958_dai_params_ssp1_fm;
+		info = &MRFLD_CONFIG_SLOT(0x03, 0x03, 2, SNDRV_PCM_FORMAT_S16_LE);
+	}
+	ret = merr_set_slot_and_format(dai, info, fmt);
+
+	return ret;
+}
+
+static int mrfld_modem_fixup(struct snd_soc_dai_link *dai_link, struct snd_soc_dai *dai)
+{
+	int ret;
+	unsigned int fmt;
+	struct mrfld_slot_info *info;
+
+	info = &MRFLD_CONFIG_SLOT(0x01, 0x01, 1, SNDRV_PCM_FORMAT_S16_LE);
+	fmt = SND_SOC_DAIFMT_DSP_A | SND_SOC_DAIFMT_IB_NF
+						| SND_SOC_DAIFMT_CBS_CFS;
+
+	ret = merr_set_slot_and_format(dai, info, fmt);
+
+	return ret;
+}
+
+static int mrfld_codec_loop_fixup(struct snd_soc_dai_link *dai_link, struct snd_soc_dai *dai)
+{
+	int ret;
+	unsigned int fmt;
+	struct mrfld_slot_info *info;
+
+	info = &MRFLD_CONFIG_SLOT(0xF, 0xF, 4, SNDRV_PCM_FORMAT_S24_LE);
+	fmt = SND_SOC_DAIFMT_DSP_B | SND_SOC_DAIFMT_IB_NF
+					| SND_SOC_DAIFMT_CBS_CFS;
+	ret = merr_set_slot_and_format(dai, info, fmt);
+
+	return ret;
 }
 
 static int mrfld_8958_set_bias_level(struct snd_soc_card *card,
@@ -714,9 +849,8 @@ struct snd_soc_dai_link mrfld_8958_msic_dailink[] = {
 		.platform_name = "sst-platform",
 		.codec_dai_name = "wm8994-aif1",
 		.codec_name = "wm8994-codec",
-		.dai_fmt = SND_SOC_DAIFMT_DSP_B | SND_SOC_DAIFMT_IB_NF
-						| SND_SOC_DAIFMT_CBS_CFS,
-		.params = &mrfld_wm8958_dai_params,
+		.params = &mrfld_wm8958_dai_params_ssp2,
+		.be_fixup = mrfld_codec_loop_fixup,
 		.dsp_loopback = true,
 	},
 	{
@@ -726,7 +860,8 @@ struct snd_soc_dai_link mrfld_8958_msic_dailink[] = {
 		.platform_name = "sst-platform",
 		.codec_dai_name = "snd-soc-dummy-dai",
 		.codec_name = "snd-soc-dummy",
-		.params = &mrfld_wm8958_dai_params,
+		.params = &mrfld_wm8958_dai_params_ssp0,
+		.be_fixup = mrfld_modem_fixup,
 		.dsp_loopback = true,
 	},
 	{
@@ -736,7 +871,8 @@ struct snd_soc_dai_link mrfld_8958_msic_dailink[] = {
 		.platform_name = "sst-platform",
 		.codec_dai_name = "snd-soc-dummy-dai",
 		.codec_name = "snd-soc-dummy",
-		.params = &mrfld_wm8958_dai_params,
+		.params = &mrfld_wm8958_ssp1_bt_nb,
+		.be_fixup = mrfld_bt_fm_fixup,
 		.dsp_loopback = true,
 	},
 
