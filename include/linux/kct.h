4dbc0ce0cd8d6d4d0549df34c36805d57ed4d748
Traian Schiau <traianx.schiau@intel.com>
2014-06-18 10:27:00 +0300

    kct: Add support for tp2e additional steps
    BZ: 203298
    
    Add a new parameter to trigger standard additional
    steps (eg. attach aplog, fw_log).
    
    Change-Id: I8c0adf86ad8482ede47436fc299d0b53c9c73550
    Signed-off-by: Traian Schiau <traianx.schiau@intel.com>

--- include/linux/kct.h
+++ include/linux/kct.h
@@ -40,8 +40,14 @@ enum ct_attchmt_type {
 	CT_ATTCHMT_DATA5,
 	/* Always add new types after DATA5 */
 	CT_ATTCHMT_BINARY,
-	CT_ATTCHMT_FILELIST
+	CT_ATTCHMT_FILELIST,
+	CT_ATTCHMT_ADDITIONAL
 };
+
+#define CT_ADDITIONAL_NONE         0
+#define CT_ADDITIONAL_APLOG        (1<<0)
+#define CT_ADDITIONAL_LAST_KMSG    (1<<1)
+#define CT_ADDITIONAL_FWMSG        (1<<2)
 
 struct ct_attchmt {
 	__u32 size; /* sizeof(data) */
@@ -107,8 +113,8 @@ extern void kct_free_event(struct ct_event *ev) __weak;
 extern int kct_log_event(struct ct_event *ev, gfp_t flags) __weak;
 
 /* API */
-#define MKFN(fn, ...) MKFN_N(fn, ##__VA_ARGS__, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)(__VA_ARGS__)
-#define MKFN_N(fn, n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n, ...) fn##n
+#define MKFN(fn, ...) MKFN_N(fn, ##__VA_ARGS__, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)(__VA_ARGS__)
+#define MKFN_N(fn, n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n, ...) fn##n
 #define kct_log(...) MKFN(__kct_log_, ##__VA_ARGS__)
 
 #define __kct_log_4(Type, Submitter_name, Ev_name, flags) \
@@ -275,6 +281,43 @@ extern int kct_log_event(struct ct_event *ev, gfp_t flags) __weak;
 			if (filelist) \
 			kct_add_attchmt(&__ev, CT_ATTCHMT_FILELIST, \
 					strlen(filelist) + 1, filelist, GFP_ATOMIC); \
+			kct_log_event(__ev, GFP_ATOMIC); \
+		} \
+	} } while (0)
+
+	#define __kct_log_12(Type, Submitter_name, Ev_name, flags, Data0, Data1, Data2, \
+					 Data3, Data4, Data5, filelist, add_steps) \
+	do {  if (kct_alloc_event) {	\
+		struct ct_event *__ev =	\
+			kct_alloc_event(Submitter_name, Ev_name, Type, \
+				GFP_ATOMIC, flags); \
+		if (__ev) { \
+			uint32_t add_steps_32 = add_steps;\
+			if (Data0) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA0, \
+					strlen(Data0) + 1, Data0, GFP_ATOMIC); \
+			if (Data1) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA1, \
+					strlen(Data1) + 1, Data1, GFP_ATOMIC); \
+			if (Data2) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA2, \
+					strlen(Data2) + 1, Data2, GFP_ATOMIC); \
+			if (Data3) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA3, \
+					strlen(Data3) + 1, Data3, GFP_ATOMIC); \
+			if (Data4) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA4, \
+					strlen(Data4) + 1, Data4, GFP_ATOMIC); \
+			if (Data5) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA5, \
+					strlen(Data5) + 1, Data5, GFP_ATOMIC); \
+			if (filelist) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_FILELIST, \
+					strlen(filelist) + 1, filelist, GFP_ATOMIC); \
+			if (add_steps) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_ADDITIONAL, \
+					sizeof(uint32_t), \
+					(char *) &add_steps_32, GFP_ATOMIC); \
 			kct_log_event(__ev, GFP_ATOMIC); \
 		} \
 	} } while (0)
