d75ffdcbe1cfbc07d6e072cc5aef14f63af39a94
Yann Fouassier <yann.fouassier@intel.com>
2014-06-27 10:53:49 +0200

    npk: update npk.h with new IOCTLs commands
    BZ: 207671
    
    Change-Id: Id3fd3fbdc7a21054b0971532f5199af3bb5f7ccd
    Signed-off-by: Yann Fouassier <yann.fouassier@intel.com>

--- include/linux/npk.h
+++ include/linux/npk.h
@@ -28,6 +28,10 @@
 /* ioctl command to print debug info on NPK trace buffer */
 #define NPKIOC_PRINT_MSU_INFO   _IOW(NPK_IOCTL_MAGIC, 7, u32)
 
+/* ioctl commands to start/stop/configure trace */
+#define NPKIOC_START_TRACE      _IO(NPK_IOCTL_MAGIC, 1)
+#define NPKIOC_STOP_TRACE       _IO(NPK_IOCTL_MAGIC, 2)
+#define NPKIOC_CONFIGURE_TRACE  _IOW(NPK_IOCTL_MAGIC, 8, struct npk_cfg)
 
 union dn_sth_reg {
 	u8 d8;
@@ -87,6 +91,28 @@ struct npk_mem_cmd {
 	u64 data;
 	u32 size;
 	u32 pad;
+};
+
+/* npk_cfg struct holds the NPK trace configuration (sources, destination):
+ *   - swdest_bmp[n], n in [0..31], is a bitmap for Master IDs [8n..(8n+7)]
+ *     to enable Master ID N, do swdest_bmp[N << 3] |= (1 << (N & 0x7))
+ *   - gswtdest: indicates if SW Master IDs 256+ shall be enabled (0: no, 1:yes)
+ *   - output_port: indicates the destination port (0: MSC0, 1: MSC1, 2: PTI)
+ *   - pti_clk_divider: for PTI clock, use NorthPeak clock divided by 2^n, n in [0..3]
+ *   - pti_mode: PTI port output mode (1: 4-bit, 2: 8-bit, 3: 12-bit, 4: 16-bit)
+ *   - pti_pattern: PTI training/calibration pattern ID to use (0 means no pattern)
+ *   - msc_mode: MSCn mode (0: CSR, 1: multi-block, 2: ExI, 3: MTB)
+ *   - msc_wrap_en: indicates if wrapping is enabled for MSCn
+ */
+struct npk_cfg {
+	u8 swdest_bmp[32];
+	u8 gswtdest;
+	u8 output_port;
+	u8 pti_clk_divider;
+	u8 pti_mode;
+	u8 pti_pattern;
+	u8 msc_mode;
+	u8 msc_wrap_en;
 };
 
 void *npk_alloc_sth_sven_ptr(int cpu);
