cdb3af2a69dd98b30b504a4aafdfc7ef2219ce5b
Mythri P K <mythri.p.k@intel.com>
2014-07-04 23:34:57 +0530

    Audio: MOFD: Vibra: Remove spid check for MOOR
    BZ: 208792
    
    MOFD V1 variant has Vibra DRV2603 and it  does not allow
    I2C writes. Disabling vibra calibration which does I2C writes
    is based on ext_drv setting, which is done in moor_pr_vibra_pci_data.
    Hence based on PCI ID and INTEL_MID_BOARD check disable ext_drv setting.
    This is needed to prevent futile vibra calibration effort which
    results in increased cold boot latency.
    
    Change-Id: I6b3a297e477da37f081245f087bd5897065e3cf7
    Signed-off-by: Mythri P K <mythri.p.k@intel.com>

--- arch/x86/platform/intel-mid/device_libs/pci/platform_mid_vibra.c
+++ arch/x86/platform/intel-mid/device_libs/pci/platform_mid_vibra.c
@@ -73,12 +73,12 @@ static struct mid_vibra_pdata *get_vibra_platform_data(struct pci_dev *pdev)
 		}
 		break;
 	case PCI_DEVICE_ID_INTEL_VIBRA_MOOR:
-		/* Mountain Praire board uses DRV2605, same as Saltbay */
-		if (SPID_HARDWARE_ID(MOFD, PHONE, MP, VVA) ||
-				SPID_HARDWARE_ID(MOFD, PHONE, MP, VVB))
+		/* Mofd v1 has vibra DRV2603 */
+		if (INTEL_MID_BOARD(2, PHONE, MOFD, V1, PRO) ||
+			INTEL_MID_BOARD(2, PHONE, MOFD, V1, ENG))
+			pdata = &moor_pr_vibra_pci_data;
+		else /* Mofd V0 has Vibra DRV2605 */
 			pdata = &mrfld_vibra_pci_data;
-		else
-			pdata = &moor_pr_vibra_pci_data; /*PR uses DRV2603*/
 
 		pdata->gpio_en = get_gpio_by_name("haptics_en");
 		pdata->gpio_pwm = get_gpio_by_name("haptics_pwm");
