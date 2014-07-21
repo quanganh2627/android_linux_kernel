68dd8369ae1c5618d846c6de8d1425caf92ab103
Ramalingam C <ramalingam.c@intel.com>
2014-07-21 22:03:52 +0530

    drm/i915: Default DRRS idleness detection interval modification
    BZ: 217066
    
    Setting the default DRRS idleness detection interval to 180ms
    (i.e ~10 Frames at 60Hz)
    
    Change-Id: Ie72acddb5e2c30c8e9b0d61870fd8c65f6b22384
    Signed-off-by: Ramalingam C <ramalingam.c@intel.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -243,10 +243,10 @@ module_param_named(i915_enable_cmd_parser, i915_enable_cmd_parser, int, 0600);
 MODULE_PARM_DESC(i915_enable_cmd_parser,
 		"Enable command parsing (default: true)");
 
-int i915_drrs_interval __read_mostly = 2000;
+int i915_drrs_interval __read_mostly = 180;
 module_param_named(drrs_interval, i915_drrs_interval, int, 0600);
 MODULE_PARM_DESC(drrs_interval,
-	"DRRS idleness detection interval (default: 2000 ms)."
+	"DRRS idleness detection interval (default: 180 ms)."
 	"If this field is set to 0, then seamless DRRS feature "
 	"based on idleness detection is disabled."
 	"The interval is to be set in milliseconds.");
