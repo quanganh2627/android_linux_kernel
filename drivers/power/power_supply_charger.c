d147e3b1517678ebca375b27eba3d22ecccf436f
Venkataramana Kotakonda <venkataramana.kotakonda@intel.com>
2014-09-08 17:28:28 +0530

    EM/FW: Fixed charger online status update issue.
    Description: During charger fault condition,  charging framework
    is not setting charger online to 0 if charger is disabled which
    results unexpected behavior. This patch will fix this issue.
    
    Change-Id: Ieb74b93830c8f3b9e330652ee515e7266050a542
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-1046
    Signed-off-by: Venkataramana Kotakonda <venkataramana.kotakonda@intel.com>
    Reviewed-on: https://android.intel.com:443/247257

--- drivers/power/power_supply_charger.c
+++ drivers/power/power_supply_charger.c
@@ -666,7 +666,7 @@ static void update_sysfs(struct power_supply *psy)
 			if (!IS_PRESENT(chrgr_lst[cnt]))
 				continue;
 
-			update_charger_online(psy);
+			update_charger_online(chrgr_lst[cnt]);
 		}
 		/* set battery status */
 		if (set_battery_status(psy, get_battery_status(psy)))
@@ -687,7 +687,7 @@ static void update_sysfs(struct power_supply *psy)
 					 * forcefully cache the battery
 					 * properties
 					 */
-					cache_cur_batt_prop_force(psy);
+					cache_cur_batt_prop_force(psb);
 		}
 	}
 }
