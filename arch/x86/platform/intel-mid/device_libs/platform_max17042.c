7127f667824ae20b552e5d58a80ab13207b97418
ashuto1x <ashutoshx.kumar@intel.com>
2014-11-12 11:05:02 +0530

    EM/Charger: KW Issue needs to be fixed for BANNED functions
    [Description]
    KW Issue needs to be fixed for BANNED functions
    
    [Implementation]
    The KW issue fix is provided to resolve the
    BANNED functions to be replaced from sprintf to snprintf,
    memcpy to memcpy_s
    
    Change-Id: I699ed7e45d5d227ad509ca73d4f47db8e1aac953
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-8746
    Signed-off-by: ashuto1x <ashutoshx.kumar@intel.com>
    Reviewed-on: https://android.intel.com:443/299828

--- arch/x86/platform/intel-mid/device_libs/platform_max17042.c
+++ arch/x86/platform/intel-mid/device_libs/platform_max17042.c
@@ -128,14 +128,21 @@ static bool msic_battery_check(struct max17042_platform_data *pdata)
 				pdata->battid[1] >= '0'
 					&& pdata->battid[1] <= '9') {
 				unsigned char tmp[SERIAL_NUM_LEN + 2];
-				int i;
+				int i, ret = 0;
 				snprintf(pdata->model_name,
 					(MODEL_NAME_LEN) + 1,
 						"%s", pdata->battid);
-				memcpy(tmp, sb->pentry, BATTID_LEN);
+				ret = memcpy_safe(tmp, BATTID_LEN, sb->pentry,
+							BATTID_LEN);
+				if (ret) {
+					pr_err("%s, err:%d copying BATTID\n",
+						__func__, ret);
+					return false;
+				}
 				for (i = 0; i < SERIAL_NUM_LEN; i++) {
-					sprintf(pdata->serial_num + i*2,
-					"%02x", tmp[i + MODEL_NAME_LEN]);
+					snprintf(pdata->serial_num + i*2,
+						sizeof(pdata->serial_num) - i*2,
+						"%02x", tmp[i + MODEL_NAME_LEN]);
 				}
 				if ((2 * SERIAL_NUM_LEN) <
 					ARRAY_SIZE(pdata->serial_num))
@@ -215,12 +222,20 @@ static void ctp_fg_restore_config_data(const char *name, void *data, int len)
 {
 	struct max17042_config_data *fg_cfg_data =
 				(struct max17042_config_data *)data;
+	int ret = 0;
 	fg_cfg_data->cfg = 0x2210;
 	fg_cfg_data->learn_cfg = 0x0076;
 	fg_cfg_data->filter_cfg = 0x87a4;
 	fg_cfg_data->relax_cfg = 0x506b;
-	memcpy(&fg_cfg_data->cell_char_tbl, ctp_cell_char_tbl,
-					sizeof(ctp_cell_char_tbl));
+	ret = memcpy_safe(&fg_cfg_data->cell_char_tbl,
+			sizeof(fg_cfg_data->cell_char_tbl),
+			ctp_cell_char_tbl,
+			sizeof(ctp_cell_char_tbl));
+	if (ret) {
+		pr_err("%s, err:%d in copying cell char tbl\n",
+			__func__, ret);
+		return ret;
+	}
 	fg_cfg_data->rcomp0 = 0x0047;
 	fg_cfg_data->tempCo = 0x1920;
 	fg_cfg_data->etc = 0x00e0;
